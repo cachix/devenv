c101aabc8ab69074aa053651136c1a8efb4c66c4
Domen Kožar <domen@dev.si>
2022-10-29 11:14:51 +0100

    starting processes works!

--- devenv.nix
+++ devenv.nix
@@ -3,6 +3,8 @@
 {
   packages = [ (import ./src/devenv.nix { inherit pkgs; }) ];
 
+  processes.whileloop.exec = "while true; do echo waiting; sleep 1; done";
+
   enterShell = ''
     echo hola
   '';
