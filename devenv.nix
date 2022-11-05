cdbb53c553525aed77f12453d39865347b066a11
Domen Kožar <domen@dev.si>
2022-11-05 23:03:41 +0000

    generate options as a script and pin nix properly

--- devenv.nix
+++ devenv.nix
@@ -26,6 +26,12 @@
     # TODO: test direnv integration
     # TODO: test DIRENV_ACTIVE
   '';
+  scripts."generate-doc-options".exec = ''
+    options=$(nix build --extra-experimental-features 'flakes nix-command' --print-out-paths --no-link '.#devenv-docs-options')
+    echo "# devenv.nix options" > docs/reference/options.md
+    echo >> docs/reference/options.md
+    cat $options >> docs/reference/options.md
+  '';
 
   pre-commit.hooks = {
     nixpkgs-fmt.enable = true;
