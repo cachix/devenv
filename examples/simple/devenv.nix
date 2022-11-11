4d0f5b7240879bc9de83683f4be82a5c146872b6
Domen Kožar <domen@dev.si>
2022-11-11 08:01:53 +0000

    make simple example a bit more complete

--- examples/simple/devenv.nix
+++ examples/simple/devenv.nix
@@ -1,12 +1,26 @@
 { pkgs, ... }:
 
 {
-  env.TESTING = 1;
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
 
+  # https://devenv.sh/packages/
   packages = [ pkgs.git ];
 
   enterShell = ''
-    echo "hello from devenv :)"
+    hello
     git --version
   '';
+
+  # https://devenv.sh/languages/
+  languages.nix.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
 }
