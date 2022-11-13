1f31c29abd03c637849bb4abc092aa0dba96e5e9
Justin Wood <me@ankhers.dev>
2022-11-13 10:48:35 -0500

    Init language Elixir

---
+++ src/modules/languages/elixir.nix
@@ -0,0 +1,29 @@
+{ pkgs, config, lib, ... }:
+
+let
+  cfg = config.languages.elixir;
+in
+{
+  options.languages.elixir = {
+    enable = lib.mkEnableOption "Enable tools for Elixir development.";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      description = "Which package of Elixir to use";
+      default = pkgs.elixir;
+      defaultText = "pkgs.elixir";
+    };
+  };
+
+  config = lib.mkIf cfg.enable
+    {
+      packages = with pkgs; [
+        cfg.package
+        elixir_ls
+      ];
+
+      enterShell = ''
+        elixir --version
+      '';
+    };
+}
