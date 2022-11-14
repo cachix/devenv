eae45b61174ebb782c6090debf31cf000d105150
Domen Kožar <domen@dev.si>
2022-11-14 17:17:17 +0000

    Language: add Clojure

---
+++ src/modules/languages/clojure.nix
@@ -0,0 +1,21 @@
+{ pkgs, config, lib, ... }:
+
+let
+  cfg = config.languages.clojure;
+in
+{
+  options.languages.clojure = {
+    enable = lib.mkEnableOption "Enable tools for Clojure development.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    packages = with pkgs; [
+      clojure
+      clojure-lsp
+    ];
+
+    enterShell = ''
+      clojure --version
+    '';
+  };
+}
