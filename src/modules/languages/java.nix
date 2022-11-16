84510aeade35a5b9e0a9061493fc79f731d19d52
Bob van der Linden <bobvanderlinden@gmail.com>
2022-11-16 10:43:28 +0100

    java: add documentation to options

--- src/modules/languages/java.nix
+++ src/modules/languages/java.nix
@@ -2,24 +2,46 @@
 
 let
   cfg = config.languages.java;
-  inherit (lib) types mkEnableOption mkOption mkDefault mkIf optional;
+  inherit (lib) types mkEnableOption mkOption mkDefault mkIf mdDoc optional literalExpression;
 in
 {
   options.languages.java = {
-    enable = mkEnableOption "Enable tools for Java development.";
-    jdk.package = mkOption { type = types.package; };
+    enable = mkEnableOption "tools for Java development";
+    jdk.package = mkOption {
+      type = types.package;
+      example = pkgs.jdk8;
+      default = pkgs.jdk;
+      defaultText = literalExpression "pkgs.jdk";
+      description = mdDoc ''
+        The JDK package to use.
+        This will also become available as `JAVA_HOME`.
+      '';
+    };
     maven = {
       enable = mkEnableOption "maven";
-      package = mkOption { type = types.package; };
+      package = mkOption {
+        type = types.package;
+        defaultText = "pkgs.maven.override { jdk = cfg.jdk.package; }";
+        description = mdDoc ''
+          The maven package to use.
+          The maven package by default inherits the JDK from `languages.java.jdk.package`.
+        '';
+      };
     };
     gradle = {
       enable = mkEnableOption "gradle";
-      package = mkOption { type = types.package; };
+      package = mkOption {
+        type = types.package;
+        default = pkgs.gradle;
+        defaultText = literalExpression "pkgs.gradle";
+        description = ''
+          The gradle package to use.
+        '';
+      };
     };
   };
 
   config = mkIf cfg.enable {
-    languages.java.jdk.package = mkDefault pkgs.jdk;
     languages.java.maven.package = mkDefault (pkgs.maven.override { jdk = cfg.jdk.package; });
     packages = (optional cfg.enable cfg.jdk.package)
       ++ (optional cfg.maven.enable cfg.maven.package)
