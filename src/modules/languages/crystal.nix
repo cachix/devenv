c5def635cd1d414f7354bd5f8b988e369834592d
Brian J. Cardiff <bcardiff@gmail.com>
2022-11-18 12:30:55 -0300

    (Re)add Crystal

---
+++ src/modules/languages/crystal.nix
@@ -0,0 +1,20 @@
+{ pkgs, config, lib, ... }:
+
+let
+  cfg = config.languages.crystal;
+in
+{
+  options.languages.crystal = {
+    enable = lib.mkEnableOption "Enable tools for Crystal development.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    packages = [
+      pkgs.shards
+    ];
+
+    enterShell = ''
+      shards --version
+    '';
+  };
+}
