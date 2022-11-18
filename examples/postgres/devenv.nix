8a5170d7af35cef9a43a2478d037c2c4564cc119
Jan Likar <jan.likar@protonmail.com>
2022-11-18 18:57:28 +0100

    Show how to use psql with devenv

--- examples/postgres/devenv.nix
+++ examples/postgres/devenv.nix
@@ -2,4 +2,7 @@
 
 {
   postgres.enable = true;
+
+  # A shorthand for running psql connected to the devenv PostgreSql server.
+  scripts.psql.exec = "${pkgs.postgresql}/bin/psql -h $(realpath .devenv/state/postgres/) $@";
 }
