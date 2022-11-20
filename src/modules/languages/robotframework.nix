58ad65ff18c3526860a59aa2cce24f4183020f4f
Asko Soukka <asko.soukka@iki.fi>
2022-11-20 19:12:54 +0200

    language: robotframework

---
+++ src/modules/languages/robotframework.nix
@@ -0,0 +1,30 @@
+{ pkgs, config, lib, ... }:
+
+let
+  cfg = config.languages.robotframework;
+in
+{
+  options.languages.robotframework = {
+    enable = lib.mkEnableOption "Enable tools for Robot Framework development.";
+
+    python = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.python3;
+      defaultText = "pkgs.python3";
+      description = "The Python package to use.";
+    };
+
+  };
+
+  config = lib.mkIf cfg.enable {
+    packages = with pkgs; [
+      (cfg.python.withPackages(ps: [
+        ps.robotframework
+      ]))
+    ];
+
+    enterShell = ''
+      robot --version
+    '';
+  };
+}
