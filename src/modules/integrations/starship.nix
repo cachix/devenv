818c0541c984796d6ec0afb5634872c6a7c9bb4e
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2022-12-20 23:10:40 +0100

    add starship integration

---
+++ src/modules/integrations/starship.nix
@@ -0,0 +1,45 @@
+{ pkgs, config, lib, ... }:
+
+{
+  options.starship = {
+    enable = lib.mkEnableOption "Enable the Starship command prompt.";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.starship;
+      defaultText = "pkgs.starship";
+      description = "The Starship package to use.";
+    };
+
+    config.enable = lib.mkEnableOption "Enable Starship config override.";
+
+    config.path = lib.mkOption {
+      type = lib.types.path;
+      default = config.env.DEVENV_ROOT + "/starship.toml";
+      defaultText = "\${config.env.DEVENV_ROOT}/starship.toml";
+      description = "The starship configuration file to use.";
+    };
+  };
+
+  config = lib.mkIf config.starship.enable {
+    packages = [
+      config.starship.package
+    ];
+
+    enterShell =
+      let
+        starshipConfExport =
+          if config.starship.config.enable
+          then ''export STARSHIP_CONFIG=${config.starship.config.path}''
+          else '''';
+      in
+      starshipConfExport + ''
+        		  # Identify user's terminal to call the appropiate 'starship init' command
+                  _DEVENV_SHELL=$(ps -o cmd= $$ | awk '{print $1}')
+
+                  eval "$(starship init $_DEVENV_SHELL)"
+
+                  unset _DEVENV_SHELL
+      '';
+  };
+}
