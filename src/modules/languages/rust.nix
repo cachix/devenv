75a214b7d2ebb7cf5f5a9d5c3bf71aa5b2911184
Simon Žlender <pub.git@zlender.si>
2022-12-21 23:12:57 +0100

    rust: overridable fenix packages

--- src/modules/languages/rust.nix
+++ src/modules/languages/rust.nix
@@ -1,8 +1,10 @@
 { pkgs, config, lib, inputs, ... }:
 
 let
-  inherit (lib.attrsets) attrValues getAttrs;
+  inherit (lib.attrsets) attrValues genAttrs getAttrs;
+
   cfg = config.languages.rust;
+  tools = [ "rustc" "cargo" "rustfmt" "clippy" "rust-analyzer" ];
   setup = ''
     inputs:
       fenix:
@@ -17,8 +19,14 @@ in
     enable = lib.mkEnableOption "Enable tools for Rust development.";
 
     packages = lib.mkOption {
-      type = lib.types.attrsOf lib.types.package;
-      default = { inherit (pkgs) rustc cargo rustfmt clippy rust-analyzer; };
+      type = lib.types.submodule ({ ... }: {
+        options = genAttrs tools (name: lib.mkOption {
+          type = lib.types.package;
+          default = pkgs.${name};
+          defaultText = "pkgs.${name}";
+          description = "${name} package";
+        });
+      });
       defaultText = "pkgs";
       description = "Attribute set of packages including rustc and cargo";
     };
@@ -32,7 +40,7 @@ in
 
   config = lib.mkMerge [
     (lib.mkIf cfg.enable {
-      packages = attrValues (getAttrs [ "rustc" "cargo" "rustfmt" "clippy" "rust-analyzer" ] cfg.packages);
+      packages = attrValues (getAttrs tools cfg.packages);
       pre-commit.tools.cargo = lib.mkForce cfg.packages.cargo;
       pre-commit.tools.rustfmt = lib.mkForce cfg.packages.rustfmt;
       pre-commit.tools.clippy = lib.mkForce cfg.packages.clippy;
@@ -46,7 +54,7 @@ in
         rustPackages = fenix.packages.${pkgs.system}.${cfg.version} or (throw "languages.rust.version is set to ${cfg.version}, but should be one of: stable, beta or latest.");
       in
       {
-        languages.rust.packages = rustPackages;
+        languages.rust.packages = genAttrs tools (package: lib.mkDefault rustPackages.${package});
         env.RUST_SRC_PATH = "${inputs.fenix.packages.${pkgs.system}.${cfg.version}.rust-src}/lib/rustlib/src/rust/library";
       }
     ))
