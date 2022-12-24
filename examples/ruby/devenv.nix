677fd9cb3c1cd0a7f28dd355d86e2b0ac5450c3b
Krisztian Szabo <krisztian@deep-impact.ch>
2022-12-24 17:26:25 +0100

    Add languages.ruby.compilers.{enable,packages} options

--- examples/ruby/devenv.nix
+++ examples/ruby/devenv.nix
@@ -1,9 +1,23 @@
 { pkgs, ... }:
 
 {
-  languages.ruby.enable = true;
+  languages.ruby = {
+    enable = true;
+    compilers.enable = true;
+  };
 
   enterShell = ''
+    echo 'Making sure the basics for native compilation are available:'
+
+    which gcc
+    gcc --version
+
+    which clang
+    clang --version
+
+    which make
+    make --version
+
     bundle
   '';
 }
