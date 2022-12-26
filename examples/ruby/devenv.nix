cadd736adbf12a5bf9c838d754e12885d0f6a676
Krisztian Szabo <krisztian@deep-impact.ch>
2022-12-26 12:34:53 +0100

    Move C related packages to language module, enable for Ruby by default

--- examples/ruby/devenv.nix
+++ examples/ruby/devenv.nix
@@ -3,6 +3,9 @@
 {
   languages.ruby.enable = true;
 
+  # turn off C tooling if you do not intend to compile native extensions, enabled by default
+  # languages.c.enable = false;
+
   enterShell = ''
     echo 'Making sure the basics for native compilation are available:'
 
