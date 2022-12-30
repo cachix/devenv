a84deeb77ca54800af1dc8df20d361b0f8743f94
jonathandion <jonathandionalary@gmail.com>
2022-12-30 08:47:24 -0500

    feature/add-deno - fix lang order

--- examples/supported-languages/devenv.nix
+++ examples/supported-languages/devenv.nix
@@ -5,6 +5,7 @@
   languages.clojure.enable = true;
   languages.cplusplus.enable = true;
   languages.cue.enable = true;
+  languages.deno.enable = true;
   languages.dotnet.enable = true;
   languages.elixir.enable = true;
   languages.elm.enable = true;
@@ -29,7 +30,6 @@
   languages.scala.enable = true;
   languages.terraform.enable = true;
   languages.typescript.enable = true;
-  languages.deno.enable = true;
   languages.v.enable = true;
 
   # If you're missing a language, please contribute it by following examples of other languages <3
