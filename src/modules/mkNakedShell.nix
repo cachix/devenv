5fa8587cff20c354a738f95b4634aa16a7c1bf38
Soner Sayakci <s.sayakci@shopware.com>
2023-01-02 18:05:32 +0100

    add workaround for tmpdir errors on Darwin, fixes #255

--- src/modules/mkNakedShell.nix
+++ src/modules/mkNakedShell.nix
@@ -26,6 +26,7 @@
 , coreutils
 , system
 , writeTextFile
+, pkgs
 , lib
 }:
 let
@@ -75,7 +76,7 @@ let
     shellHook = ''
       # Remove all the unnecessary noise that is set by the build env
       unset NIX_BUILD_TOP NIX_BUILD_CORES NIX_STORE
-      unset TEMP TEMPDIR TMP TMPDIR
+      unset TEMP TEMPDIR TMP ${lib.optionalString (!pkgs.stdenv.isDarwin) "TMPDIR"}
       # $name variable is preserved to keep it compatible with pure shell https://github.com/sindresorhus/pure/blob/47c0c881f0e7cfdb5eaccd335f52ad17b897c060/pure.zsh#L235
       unset builder out shellHook stdenv system
       # Flakes stuff
