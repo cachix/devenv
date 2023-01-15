570604e502fd55704c572964e219547eedf8f18a
Andrey Kuznetsov <fear@loathing.in>
2023-01-15 15:38:33 +0400

    rust: link frameworks on darwin only if rust is enabled

--- src/modules/languages/rust.nix
+++ src/modules/languages/rust.nix
@@ -48,22 +48,25 @@ in
   };
 
   config = lib.mkMerge [
-    (lib.mkIf cfg.enable {
-      packages = attrValues (getAttrs tools cfg.packages) ++ lib.optional pkgs.stdenv.isDarwin pkgs.libiconv;
+    (lib.mkIf cfg.enable
+      {
+        packages = attrValues (getAttrs tools cfg.packages) ++ lib.optional pkgs.stdenv.isDarwin pkgs.libiconv;
 
-      # enable compiler tooling by default to expose things like cc
-      languages.c.enable = lib.mkDefault true;
+        # enable compiler tooling by default to expose things like cc
+        languages.c.enable = lib.mkDefault true;
 
-      env.RUST_SRC_PATH = cfg.packages.rust-src;
+        env.RUST_SRC_PATH = cfg.packages.rust-src;
 
-      pre-commit.tools.cargo = lib.mkForce cfg.packages.cargo;
-      pre-commit.tools.rustfmt = lib.mkForce cfg.packages.rustfmt;
-      pre-commit.tools.clippy = lib.mkForce cfg.packages.clippy;
-    })
-    (lib.mkIf pkgs.stdenv.isDarwin {
-      env.RUSTFLAGS = [ "-L framework=${config.env.DEVENV_PROFILE}/Library/Frameworks" ];
-      env.RUSTDOCFLAGS = [ "-L framework=${config.env.DEVENV_PROFILE}/Library/Frameworks" ];
-    })
+        pre-commit.tools.cargo = lib.mkForce cfg.packages.cargo;
+        pre-commit.tools.rustfmt = lib.mkForce cfg.packages.rustfmt;
+        pre-commit.tools.clippy = lib.mkForce cfg.packages.clippy;
+      } //
+    (lib.mkIf pkgs.stdenv.isDarwin
+      {
+        env.RUSTFLAGS = [ "-L framework=${config.env.DEVENV_PROFILE}/Library/Frameworks" ];
+        env.RUSTDOCFLAGS = [ "-L framework=${config.env.DEVENV_PROFILE}/Library/Frameworks" ];
+      })
+    )
     (lib.mkIf (cfg.version != null) (
       let
         fenix = inputs.fenix or (throw "To use languages.rust.version, you need to add the following to your devenv.yaml:\n\n${setup}");
