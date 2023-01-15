dfd9c4857c4e4b910b4270c6a35af9f9ba824917
Bob van der Linden <bobvanderlinden@gmail.com>
2023-01-15 12:15:47 +0100

    languages/python: add poetry support

--- src/modules/languages/python.nix
+++ src/modules/languages/python.nix
@@ -15,18 +15,39 @@ in
     };
 
     venv.enable = lib.mkEnableOption "Python virtual environment";
+
+    poetry = {
+      enable = lib.mkEnableOption "poetry";
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = cfg.package.pkgs.poetry;
+        defaultText = lib.literalExpression "config.languages.python.package.pkgs.poetry";
+        description = "The poetry package to use.";
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    languages.python.venv.enable = lib.mkIf cfg.poetry.enable (lib.mkDefault true);
+
     packages = [
       cfg.package
-    ];
+    ] ++ (lib.optional cfg.poetry.enable cfg.poetry.package);
 
     env.PYTHONPATH = "${config.env.DEVENV_PROFILE}/${cfg.package.sitePackages}";
 
-    enterShell = lib.mkIf cfg.venv.enable ''
-      python -m venv ${config.env.DEVENV_STATE}/venv
-      source ${config.env.DEVENV_STATE}/venv/bin/activate
-    '';
+    enterShell = lib.concatStringsSep "\n" (
+      (lib.optional cfg.venv.enable ''
+        python -m venv ${config.env.DEVENV_STATE}/venv
+        source ${config.env.DEVENV_STATE}/venv/bin/activate
+      '') ++ (lib.optional cfg.poetry.enable ''
+        if [ -f pyproject.toml ]
+        then
+          poetry install --no-interaction --quiet
+        else
+          echo "No pyproject.toml found. Run 'poetry init'." >&2
+        fi
+      '')
+    );
   };
 }
