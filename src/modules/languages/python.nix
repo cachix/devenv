db3b5cda50600540a2c9696d07774eba859eb500
Bob van der Linden <bobvanderlinden@gmail.com>
2023-01-21 14:59:53 +0100

    languages/python: avoid rerunning python install for same poetry.lock

--- src/modules/languages/python.nix
+++ src/modules/languages/python.nix
@@ -51,7 +51,23 @@ in
         then
           echo "No poetry.lock found. Run 'poetry install' to create one from pyproject.toml." >&2
         else
-          poetry install --no-interaction --quiet
+          # Avoid running "poetry install" for every shell.
+          # Only run it when the "poetry.lock" file has changed.
+          # We do this by storing a hash of "poetry.lock" in venv.
+          ACTUAL_POETRY_CHECKSUM="$(${pkgs.nix}/bin/nix-hash --type sha256 poetry.lock)"
+          POETRY_CHECKSUM_FILE="${config.env.DEVENV_STATE}/poetry.lock.checksum"
+          if [ -f "$POETRY_CHECKSUM_FILE" ]
+          then
+            read -r EXPECTED_POETRY_CHECKSUM < "$POETRY_CHECKSUM_FILE"
+          else
+            EXPECTED_POETRY_CHECKSUM=""
+          fi
+
+          if [ "$ACTUAL_POETRY_CHECKSUM" != "$EXPECTED_POETRY_CHECKSUM" ]
+          then
+            poetry install --no-interaction --quiet
+            echo "$ACTUAL_POETRY_CHECKSUM" > "$POETRY_CHECKSUM_FILE"
+          fi
         fi
       '')
     );
