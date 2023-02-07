b7179ae59fef44b9272058af2725989f9ca05903
Jashandeep Sohi <jashandeep.s.sohi@gmail.com>
2023-02-06 16:11:41 -0800

    properly escape color codes to avoid long command lines wraping back on themselves
    i.e. https://askubuntu.com/questions/24358/how-do-i-get-long-command-lines-to-wrap-to-the-next-line

--- src/modules/top-level.nix
+++ src/modules/top-level.nix
@@ -72,7 +72,7 @@ in
     env.DEVENV_PROFILE = profile;
 
     enterShell = ''
-      export PS1="\e[0;34m(devenv)\e[0m ''${PS1-}"
+      export PS1="\[\e[0;34m\](devenv)\[\e[0m\] ''${PS1-}"
 
       # note what environments are active, but make sure we don't repeat them
       if [[ ! "''${DIRENV_ACTIVE-}" =~ (^|:)"$PWD"(:|$) ]]; then
