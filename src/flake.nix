3801a09e1f20684c87014af318a46b0658fb66e6
Domen Kožar <domen@dev.si>
2023-02-08 13:50:17 +0000

    Support unfree attribute in devenv.yaml

--- src/flake.nix
+++ src/flake.nix
@@ -11,31 +11,40 @@
 
     outputs = { nixpkgs, ... }@inputs:
       let
-        pkgs = import nixpkgs { system = "${pkgs.system}"; };
-        lib = pkgs.lib;
         devenv = if builtins.pathExists ./.devenv/devenv.json
           then builtins.fromJSON (builtins.readFile ./.devenv/devenv.json)
           else {};
-        toModule = path:
+        getOverlays = inputName: overlays:
+          map (overlay: let 
+              input = inputs.''${inputName} or (throw "No such input `''${inputName}` while trying to configure overlays.");
+            in input.overlays.''${overlay} or (throw "Input `''${inputName}` has no overlay called `''${overlay}`. Supported overlays: ''${nixpkgs.lib.concatStringsSep ", " (builtins.attrNames input.overlays)}")) overlays;
+        overlays = nixpkgs.lib.flatten (nixpkgs.lib.mapAttrsToList getOverlays (devenv.overlays or []));
+        pkgs = import nixpkgs {
+          system = "${pkgs.system}";
+          allowUnfree = devenv.allowUnfree or false; 
+          inherit overlays;
+        };
+        lib = pkgs.lib;
+        importModule = path:
           if lib.hasPrefix "./" path
           then ./. + (builtins.substring 1 255 path) + "/devenv.nix"
           else if lib.hasPrefix "../" path 
-          then throw "devenv: ../ is not supported for imports"
-          else let
-            paths = lib.splitString "/" path;
-            name = builtins.head paths;
-            input = inputs.''${name} or (throw "Unknown input ''${name}");
-            subpath = "/''${lib.concatStringsSep "/" (builtins.tail paths)}";
-            devenvpath = "''${input}" + subpath + "/devenv.nix";
-            in if builtins.pathExists devenvpath
-               then devenvpath
-               else throw (devenvpath + " file does not exist for input ''${name}.");
+              then throw "devenv: ../ is not supported for imports"
+              else let
+                paths = lib.splitString "/" path;
+                name = builtins.head paths;
+                input = inputs.''${name} or (throw "Unknown input ''${name}");
+                subpath = "/''${lib.concatStringsSep "/" (builtins.tail paths)}";
+                devenvpath = "''${input}" + subpath + "/devenv.nix";
+                in if builtins.pathExists devenvpath
+                  then devenvpath
+                  else throw (devenvpath + " file does not exist for input ''${name}.");
         project = pkgs.lib.evalModules {
           specialArgs = inputs // { inherit inputs pkgs; };
           modules = [
             (inputs.devenv.modules + /top-level.nix)
             { devenv.cliVersion = "${version}"; }
-          ] ++ (map toModule (devenv.imports or [])) ++ [
+          ] ++ (map importModule (devenv.imports or [])) ++ [
             ./devenv.nix
             (devenv.devenv or {})
             (if builtins.pathExists ./devenv.local.nix then ./devenv.local.nix else {})
