4f59e0c8c1ec2084a6c48f2046a9778110107500
Wilko <w-gh@wmeyer.eu>
2023-02-08 17:02:04 +0100

    added: language support for forth using the gforth implementation

---
+++ src/modules/languages/forth.nix
@@ -0,0 +1,23 @@
+{ pkgs, config, lib, ... }:
+
+let
+  cfg = config.languages.forth;
+in
+{
+  options.languages.forth = {
+    enable = lib.mkEnableOption "Enable tools for Forth development.";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.gforth;
+      defaultText = lib.literalExpression "pkgs.gforth";
+      description = "The Forth package to use, defaults to GNU projects forth implementation.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    packages = [
+      cfg.package
+    ];
+  };
+}
