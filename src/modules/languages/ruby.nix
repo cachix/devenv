21f8235bc537f13b9b751faacfb40b1a3e1d2732
Bob van der Linden <bobvanderlinden@gmail.com>
2023-02-13 10:24:44 +0100

    ruby: add support for version and versionFile using nixpkgs-ruby
    Currently the ruby package can be manually changed by the user, but it
    is often inconvenient to change it to a specific ruby version.
    nixpkgs-ruby holds package definitions (and caches) all ruby versions.
    Just like phps integration in devenv, this makes use of nixpkgs-ruby
    whenever the version option is set.
    
    In addition, since the ruby community is based mostly around rbenv,
    support for setting the version using a versionFile (.ruby-version) is
    also added. This makes use of nixpkgs-ruby's packageFromRubyVersionFile
    helper function.
    
    When when 2 or more of the version, versionFile or package properties
    are set, a conflict will show up, because multiple sources are trying to
    set the package property.

--- src/modules/languages/ruby.nix
+++ src/modules/languages/ruby.nix
@@ -1,7 +1,15 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, lib, inputs, ... }:
 
 let
   cfg = config.languages.ruby;
+
+  nixpkgs-ruby = inputs.nixpkgs-ruby or (throw ''
+    To use languages.ruby.version or languages.ruby.versionFile, you need to add the following to your devenv.yaml:
+    
+      inputs:
+        nixpkgs-ruby:
+          url: github:bobvanderlinden/nixpkgs-ruby
+  '');
 in
 {
   options.languages.ruby = {
@@ -13,11 +21,55 @@ in
       defaultText = lib.literalExpression "pkgs.ruby_3_1";
       description = "The Ruby package to use.";
     };
+
+    version = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = ''
+        The Ruby version to use.
+        This automatically sets the `languages.ruby.package` using [nixpkgs-ruby](https://github.com/bobvanderlinden/nixpkgs-ruby).
+      '';
+      example = "3.2.1";
+    };
+
+    versionFile = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+      description = ''
+        The .ruby-version file path to extract the Ruby version from.
+        This automatically sets the `languages.ruby.package` using [nixpkgs-ruby](https://github.com/bobvanderlinden/nixpkgs-ruby).
+        When the `.ruby-version` file exists in the same directory as the devenv configuration, you can use:
+        
+        ```nix
+        languages.ruby.versionFile = ./.ruby-version;
+        ```
+      '';
+      example = lib.literalExpression ''
+        ./ruby-version
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
     # enable C tooling by default so native extensions can be built
     languages.c.enable = lib.mkDefault true;
+
+    languages.ruby.package =
+      let
+        packageFromVersion = lib.mkIf (cfg.version != null) (
+          nixpkgs-ruby.packages.${pkgs.system}."ruby-${cfg.version}"
+        );
+        packageFromVersionFile = lib.mkIf (cfg.versionFile != null) (
+          nixpkgs-ruby.lib.packageFromRubyVersionFile {
+            file = cfg.versionFile;
+            system = pkgs.system;
+          }
+        );
+      in
+      lib.mkMerge [
+        packageFromVersion
+        packageFromVersionFile
+      ];
 
     packages = with pkgs; [
       cfg.package
