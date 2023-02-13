f9fe71f2bcad655445502853d7bf64ab5352aadf
Jochen Manz <j.manz@kellerkinder.de>
2023-02-13 23:11:56 +0100

    added a complete mkcert example

--- examples/mkcert/devenv.nix
+++ examples/mkcert/devenv.nix
@@ -2,8 +2,19 @@
 
 {
   mkcert.domains = [
-    "foo.de"
+    "example.com"
   ];
 
+  hostctl.hosts = {
+    "example.com" = "127.0.0.1";
+  };
+
   services.caddy.enable = true;
+  services.caddy.virtualHosts."example.com" = {
+    extraConfig = ''
+      tls ${config.env.DEVENV_STATE}/mkcert/example.com.pem ${config.env.DEVENV_STATE}/mkcert/example.com-key.pem
+
+      respond "Hello, world!"
+    '';
+  };
 }
