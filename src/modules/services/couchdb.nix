0f470f1cc808acc4674f7b419f1ea478079174e0
MSBarbieri <matheussouzabarbieri@gmail.com>
2023-02-21 15:48:30 -0300

    add settings data types

--- src/modules/services/couchdb.nix
+++ src/modules/services/couchdb.nix
@@ -5,11 +5,11 @@
 }:
 let
   cfg = config.services.couchdb;
-  types = lib.types;
-  baseDir = config.env.DEVENV_STATE + "/couchdb";
+
   settingsFormat = pkgs.formats.ini { };
   configFile = settingsFormat.generate "couchdb.ini" cfg.settings;
 
+  baseDir = config.env.DEVENV_STATE + "/couchdb";
   startScript = pkgs.writeShellScriptBin "start-couchdb" ''
     set -euo pipefail
     if [[ ! -d "${baseDir}" ]]; then
@@ -31,20 +31,53 @@ in
     enable = lib.mkEnableOption "CouchDB process";
 
     package = lib.mkOption {
-      type = types.package;
+      type = lib.types.package;
       description = "Which version of CouchDB to use";
       default = pkgs.couchdb3;
       defaultText = lib.literalExpression "pkgs.couchdb3";
     };
 
     settings = lib.mkOption {
-      type = settingsFormat.type;
-      description = ''
-        CouchDB configuration.
-        to know more about all settings, look at:
-        https://docs.couchdb.org/en/stable/config/couchdb.html
-      '';
-      default = {
+      # type = settingsFormat.type;
+      type = lib.types.submodule {
+        freeformType = settingsFormat.type;
+        options.couchdb.database_dir = lib.mkOption {
+          type = lib.types.path;
+          default = baseDir;
+          description = ''
+
+          '';
+        };
+        options.couchdb.single_node = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = ''
+
+          '';
+        };
+        options.couchdb.viewIndexDir = lib.mkOption {
+          type = lib.types.path;
+          default = baseDir;
+        };
+        options.couchdb.uriFile = lib.mkOption {
+          type = lib.types.path;
+          default = "${baseDir}/couchdb.uri";
+          description = ''
+
+          '';
+        };
+      };
+    };
+    description = ''
+      CouchDB configuration.
+      to know more about all settings, look at:
+      <link
+        xlink:href="https://docs.couchdb.org/en/stable/config/couchdb.html"
+      />
+    '';
+    default = { };
+    example = lib.literalExpression ''
+      {
         couchdb = {
           database_dir = baseDir;
           single_node = true;
@@ -52,37 +85,34 @@ in
           uriFile = "${baseDir}/couchdb.uri";
         };
         admins = {
-          admin = "admin";
+          "admin_username" = "pass";
         };
         chttpd = {
           bindAddress = "127.0.0.1";
           port = 5984;
           logFile = "${baseDir}/couchdb.log";
         };
-      };
-      example = lib.literalExpression ''
-        {
-          couchdb = {
-            database_dir = baseDir;
-            single_node = true;
-            viewIndexDir = baseDir;
-            uriFile = "${baseDir}/couchdb.uri";
-          };
-          admins = {
-            "admin_username" = "pass";
-          };
-          chttpd = {
-            bindAddress = "127.0.0.1";
-            port = 5984;
-            logFile = "${baseDir}/couchdb.log";
-          };
-        }
-      '';
-    };
+      }
+    '';
   };
-
   config = lib.mkIf cfg.enable {
     packages = [ cfg.package ];
+    services.couchdb.settings = {
+      couchdb = {
+        database_dir = baseDir;
+        single_node = true;
+        viewIndexDir = baseDir;
+        uriFile = "${baseDir}/couchdb.uri";
+      };
+      admins = {
+        admin = "admin";
+      };
+      chttpd = {
+        bindAddress = "127.0.0.1";
+        port = 5984;
+        logFile = "${baseDir}/couchdb.log";
+      };
+    };
     env.ERL_FLAGS = "-couch_ini ${configFile}";
     processes.couchdb.exec = "${startScript}/bin/start-couchdb";
   };
