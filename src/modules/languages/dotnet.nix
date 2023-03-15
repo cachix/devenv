28a4cf56df69ae1ce2c95685c5a829cad17bd0db
Victor Freire <victor@freire.dev.br>
2023-03-15 15:59:47 -0300

    refactor(dotnet): add package option and needed environment variables

--- src/modules/languages/dotnet.nix
+++ src/modules/languages/dotnet.nix
@@ -6,11 +6,21 @@ in
 {
   options.languages.dotnet = {
     enable = lib.mkEnableOption "tools for .NET development";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.dotnetCorePackages.sdk_7_0;
+      defaultText = lib.literalExpression "pkgs.dotnet-sdk";
+      description = "The .NET SDK package to use.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     packages = with pkgs; [
-      dotnet-sdk
+      cfg.package
     ];
+
+    env.DOTNET_ROOT = "${cfg.package}";
+    env.LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${lib.makeLibraryPath [ pkgs.icu ]}";
   };
 }
