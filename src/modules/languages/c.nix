364face49d2f39c79f0af4eaf937c88e8278bf97
Jörg Thalheim <joerg@thalheim.io>
2023-03-16 08:46:07 +0100

    improve c support

--- src/modules/languages/c.nix
+++ src/modules/languages/c.nix
@@ -6,16 +6,29 @@ in
 {
   options.languages.c = {
     enable = lib.mkEnableOption "tools for C development";
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.stdenv.cc;
+      example = "pkgs.clangStdenv.cc";
+      defaultText = "pkgs.stdenv.cc";
+      description = "C/C++ toolchain to use. Defaults to nixpkgs default C compiler (i.e. gcc on Linux and clang on macOS)";
+    };
+    languageServer = lib.mkOption {
+      type = lib.types.nullOr lib.types.package;
+      default = pkgs.clang-tools;
+      example = pkgs.ccls;
+      defaultText = "pkgs.clang-tools";
+      description = "Language server to use. Defaults to clangd from the clang-tools package.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     packages = with pkgs; [
-      stdenv
       gnumake
-      clang
-      ccls
-      gcc
       pkg-config
+
+      cfg.package
+      cfg.languageServer
     ];
   };
 }
