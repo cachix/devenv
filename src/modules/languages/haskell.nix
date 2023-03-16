34b6bb203fe3fc6957ec3a38ddbc07cac9fbdd00
Domen Kožar <domen@dev.si>
2023-03-16 14:18:02 +0000

    haskell: add package and haskell-language-server

--- src/modules/languages/haskell.nix
+++ src/modules/languages/haskell.nix
@@ -2,14 +2,39 @@
 
 let
   cfg = config.languages.haskell;
+  # ghc.version with removed dots
+  ghcVersion = lib.replaceStrings [ "." ] [ "" ] cfg.package.version;
 in
 {
   options.languages.haskell = {
     enable = lib.mkEnableOption "tools for Haskell development";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.ghc;
+      defaultText = "pkgs.ghc";
+      description = ''
+        Haskell compiler to use.
+      '';
+    };
+
+    languageServer = lib.mkOption {
+      type = lib.types.nullOr lib.types.package;
+      default = pkgs.haskell-language-server.override
+        {
+          supportedGhcVersions = [ ghcVersion ];
+        };
+      defaultText = "pkgs.haskell-language-server";
+      description = ''
+        Haskell language server to use.
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
     packages = with pkgs; [
+      cfg.package
+      cfg.languageServer
       stack
       cabal-install
       zlib
