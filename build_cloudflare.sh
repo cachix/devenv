764d5e7e5fb0c433e27bb8ea5bacaa3b7fcd64bb
Domen Kožar <domen@dev.si>
2023-04-04 14:38:34 +0100

    test

--- build_cloudflare.sh
+++ build_cloudflare.sh
@@ -1,3 +1,1 @@
-locale 
-pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.1.5-insiders-4.32.4
-mkdocs build --config-file mkdocs.insiders.yml
+TERM=xterm mkdocs build
