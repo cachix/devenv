6c23467c91f97999be1235253779feecf7ede9e8
Domen Kožar <domen@dev.si>
2023-04-04 14:31:36 +0100

    upgrade mkdocs-material-insiders

--- build_cloudflare.sh
+++ build_cloudflare.sh
@@ -1,3 +1,3 @@
 locale 
-pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.0.1-insiders-4.27.0
+pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.1.5-insiders-4.32.4
 mkdocs build --config-file mkdocs.insiders.yml
