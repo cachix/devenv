776d51baa939dbd2af18e07a82493e70cc6bac2d
Bob van der Linden <bobvanderlinden@gmail.com>
2023-04-05 14:23:52 +0200

    python: poetry: create virtualenv using poetry
    Whenever config.languages.python.poetry.enable is set devenv would
    previously create a .venv directory using `python -m venv`. This works
    fine, but poetry itself is also able to create virtualenv, which is
    automatically used when poetry needs a virtualenv and it does not exist.
    
    To keep virtualenv consistent it is best to create the virtualenv using
    poetry to avoid a 'devenv'-version of virtualenv and a poetry one.
    
    Since poetry also handles switching of python interpreter I opted to use
    this as well.
    
    This also decouples config.languages.python.venv.enable from
    config.languages.python.poetry.enable.

--- src/modules/languages/python.nix
+++ src/modules/languages/python.nix
@@ -22,18 +22,22 @@ let
   '';
 
   initPoetryScript = pkgs.writeShellScript "init-poetry.sh" ''
-    if [ ! -f pyproject.toml ]
-    then
-      echo "No pyproject.toml found. Run 'poetry init' to create one." >&2
-    elif [ ! -f poetry.lock ]
-    then
-      echo "No poetry.lock found. Run 'poetry install' to create one from pyproject.toml." >&2
-    else
+    function _devenv-init-poetry-venv()
+    {
+      if [ ! -d ${config.env.DEVENV_ROOT}/.venv ] \
+        || [ ! "$(readlink ${config.env.DEVENV_ROOT}/.venv/bin/python)" -ef "${cfg.package.interpreter}" ]
+      then
+        poetry env use --no-interaction ${cfg.package.interpreter}
+      fi
+    }
+
+    function _devenv-poetry-install()
+    {
       # Avoid running "poetry install" for every shell.
-      # Only run it when the "poetry.lock" file has changed.
-      # We do this by storing a hash of "poetry.lock" in venv.
-      ACTUAL_POETRY_CHECKSUM="$(${pkgs.nix}/bin/nix-hash --type sha256 poetry.lock)"
-      POETRY_CHECKSUM_FILE="${config.env.DEVENV_STATE}/poetry.lock.checksum"
+      # Only run it when the "poetry.lock" file or python interpreter has changed.
+      # We do this by storing the interpreter path and a hash of "poetry.lock" in venv.
+      local ACTUAL_POETRY_CHECKSUM="${cfg.package.interpreter}:$(${pkgs.nix}/bin/nix-hash --type sha256 poetry.lock)"
+      local POETRY_CHECKSUM_FILE="${config.env.DEVENV_ROOT}/.venv/poetry.lock.checksum"
       if [ -f "$POETRY_CHECKSUM_FILE" ]
       then
         read -r EXPECTED_POETRY_CHECKSUM < "$POETRY_CHECKSUM_FILE"
@@ -43,13 +47,23 @@ let
 
       if [ "$ACTUAL_POETRY_CHECKSUM" != "$EXPECTED_POETRY_CHECKSUM" ]
       then
-        if ${cfg.poetry.package}/bin/poetry install --no-interaction --quiet
+        if ${cfg.poetry.package}/bin/poetry install --no-interaction ${lib.concatStringsSep " " cfg.poetry.install.arguments}
         then
           echo "$ACTUAL_POETRY_CHECKSUM" > "$POETRY_CHECKSUM_FILE"
         else
           echo "Poetry install failed. Run 'poetry install' manually."
         fi
       fi
+    }
+
+    if [ ! -f pyproject.toml ]
+    then
+      echo "No pyproject.toml found. Run 'poetry init' to create one." >&2
+    else
+      _devenv-init-poetry-venv
+      ${lib.optionalString cfg.poetry.install.enable ''
+        _devenv-poetry-install
+      ''}
     fi
   '';
 in
@@ -68,6 +82,26 @@ in
 
     poetry = {
       enable = lib.mkEnableOption "poetry";
+      install = {
+        enable = lib.mkEnableOption "poetry install during devenv initialisation";
+        arguments = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [ ];
+          description = "Command line arguments pass to `poetry install` during devenv initialisation.";
+          internal = true;
+        };
+        installRootPackage = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Whether the root package (your project) should be installed. See `--no-root`";
+        };
+        quiet = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = "Whether `poetry install` should avoid outputting messages during devenv initialisation.";
+        };
+      };
+
       package = lib.mkOption {
         type = lib.types.package;
         default = pkgs.poetry.override {
@@ -84,18 +118,30 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    languages.python.venv.enable = lib.mkIf cfg.poetry.enable (lib.mkDefault true);
+    languages.python.poetry.install.enable = lib.mkIf cfg.poetry.enable (lib.mkDefault true);
+    languages.python.poetry.install.arguments =
+      lib.optional (!cfg.poetry.install.installRootPackage) "--no-root" ++
+      lib.optional cfg.poetry.install.quiet "--quiet";
 
     packages = [
       cfg.package
     ] ++ (lib.optional cfg.poetry.enable cfg.poetry.package);
 
-    env.PYTHONPATH = "${config.env.DEVENV_PROFILE}/${cfg.package.sitePackages}";
+    env = {
+      PYTHONPATH = "${config.env.DEVENV_PROFILE}/${cfg.package.sitePackages}";
+    } // (lib.optionalAttrs cfg.poetry.enable {
+      # Make poetry use DEVENV_ROOT/.venv
+      POETRY_VIRTUALENVS_IN_PROJECT = "true";
+      # Make poetry create the local virtualenv when it does not exist.
+      POETRY_VIRTUALENVS_CREATE = "true";
+      # Make poetry stop accessing any other virtualenvs in $HOME.
+      POETRY_VIRTUALENVS_PATH = "/var/empty";
+    });
 
     enterShell = lib.concatStringsSep "\n" (
       (lib.optional cfg.venv.enable ''
         source ${initVenvScript}
-      '') ++ (lib.optional cfg.poetry.enable ''
+      '') ++ (lib.optional cfg.poetry.install.enable ''
         source ${initPoetryScript}
       '')
     );
