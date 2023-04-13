f6c1b60c509eab4c1881de22d0e6544e1dbb3e81
Domen Kožar <domen@dev.si>
2023-04-13 16:16:13 +0100

    templates: remove unneeded line

--- templates/simple/flake.nix
+++ templates/simple/flake.nix
@@ -8,7 +8,6 @@
   outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
     let
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
-      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
     in
     {
       devShells = forEachSystem
