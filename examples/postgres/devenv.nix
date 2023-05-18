7e04f5c6c433ae34a0289d678235eb06deeb4afa
Sander <hey@sandydoo.me>
2023-05-18 17:24:39 +0000

    postgres: demonstrate extensions in the example

--- examples/postgres/devenv.nix
+++ examples/postgres/devenv.nix
@@ -2,4 +2,10 @@
 
 {
   services.postgres.enable = true;
+
+  # Enable the optional PostGIS extension.
+  services.postgres.extensions = extensions: [ extensions.postgis ];
+  services.postgres.initialScript = ''
+    CREATE EXTENSION IF NOT EXISTS postgis;
+  '';
 }
