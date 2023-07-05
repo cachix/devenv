f91e399e6c663b88fc2242ce0fc11859de3001ae
Colton Donnelly <colton@donn.io>
2023-07-05 10:40:28 -1000

    re-add nixpkgs by default, get some things working

--- src/modules/languages/rust.nix
+++ src/modules/languages/rust.nix
@@ -1,109 +1,99 @@
-# { pkgs, config, lib, inputs, ... }:
-{ }
+{ pkgs, config, lib, inputs, ... }:
 
-# let
-#   cfg = config.languages.rust;
-#   setup = ''
-#     inputs:
-#       fenix:
-#         url: github:nix-community/fenix
-#         inputs:
-#           nixpkgs:
-#             follows: nixpkgs
-#   '';
+let
+  cfg = config.languages.rust;
+  setup = ''
+    inputs:
+      fenix:
+        url: github:nix-community/fenix
+        inputs:
+          nixpkgs:
+            follow: nixpkgs
+  '';
 
-#   fenix-input = dbg: inputs.fenix or (throw "To use languages.rust.${dbg}, you need to add the following to your devenv.yaml:\n\n${setup}");
+  fenix' = inputs.fenix or
+    (throw "to use languages.rust, you must add the following to your devenv.yaml:\n\n${setup}");
+  fenix = fenix'.packages.${pkgs.stdenv.system};
+in
+{
+  options.languages.rust = {
+    enable = lib.mkEnableOption "tools for Rust development";
 
-#   tryPath = p: pkgs.lib.optional (pkgs.lib.pathExists p) p;
-# in
-# {
-#   options.languages.rust' = {
-#     enable = lib.mkEnableOption "tools for Rust development";
+    package = lib.mkOption {
+      type = lib.types.package;
+      description = "Rust package including rustc and Cargo.";
+      defaultText = lib.literalExpression "nixpkgs";
+      default = pkgs.symlinkJoin {
+        name = "nixpkgs-rust";
+        paths = with pkgs; [
+          rustc
+          cargo
+          rustfmt
+          clippy
+          rust-analyzer
+        ];
+      };
+    };
 
-#     components = lib.mkOption {
-#       type = lib.types.nullOr (lib.types.listOf lib.types.str);
-#       default = null;
-#       defaultText = lib.literalExpression "null";
-#       description = "Rust components to install.";
-#     };
+    components = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = ''
+        List of [Rustup components](https://rust-lang.github.io/rustup/concepts/components.html)
+        to install.
+      '';
+      default = [
+        "rustc"
+        "cargo"
+        "clippy"
+        "rustfmt"
+        "rust-analyzer"
+      ];
+      defaultText = lib.literalExpression ''[ "rust-analyzer" ]'';
+    };
 
-#     package = lib.mkOption {
-#       type = lib.types.package;
-#       defaultText = lib.literalExpression "nixpkgs";
-#       default = pkgs.symlinkJoin {
-#         name = "nixpkgs-rust";
-#         paths = with pkgs; [
-#           rustc
-#           cargo
-#           rustfmt
-#           clippy
-#           rust-analyzer
-#         ];
-#         postBuild = ''
-#           for bin in $out/bin/*; do
-#             if [[ -x $bin ]]; then
-#               wrapProgram $bin
-#                 --set RUST_SRC_PATH ${pkgs.rustPlatform.rustLibSrc}
-#             fi
-#           done
-#         '';
-#       };
-#       description = "Rust package including rustc and Cargo.";
-#     };
+    rust-src = lib.mkOption {
+      type = lib.types.path;
+      default = pkgs.rustPlatform.rustLibSrc;
+    };
 
-#     profile = lib.mkOption {
-#       type = lib.types.nullOr (lib.types.enum [ "minimal" "default" "complete" ]);
-#       description = ''
-#         The [rustup profile](https://rust-lang.github.io/rustup/concepts/profiles.html)
-#       '';
-#     };
+    toolchain = lib.mkOption {
+      # TODO: better type with https://nixos.org/manual/nixos/stable/index.html
+      type = lib.types.nullOr lib.types.anything;
+      description = ''
+        The [fenix toolchain](https://github.com/nix-community/fenix#toolchain) to use.
+      '';
+      default = null;
+      defaultText = lib.literalExpression "fenix.packages.stable";
+    };
 
-#     toolchain = lib.mkOption {
-#       type = lib.types.nullOr (
-#         lib.types.lazyAttrsOf
-#           ((fenix-input "toolchain").packages.${pkgs.stdenv.system}.stable).type
-#       );
-#       description = ''
-#         The [fenix toolchain](https://github.com/nix-community/fenix#toolchain) to use.
+    version = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      description = ''
+        The [Rustup channel](https://rust-lang.github.io/rustup/concepts/channels.html) to install.
+      '';
+      default = null;
+      defaultText = lib.literalExpression "null";
+    };
+  };
 
-#         To use fenix, add the following to your devenv.yaml:
-#         ```yaml title="devenv.yaml"
-#         ${setup}
-#         ```
-#       '';
-#       default = null;
-#       defaultText = lib.literalExpression "null";
-#     };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      packages = [ cfg.package ]
+        ++ lib.optional pkgs.stdenv.isDarwin pkgs.libiconv;
 
-#     version = lib.mkOption {
-#       type = lib.types.nullOr lib.types.str;
-#       default = null;
-#       description = "Set to stable, beta, or latest.";
-#       defaultText = "null";
-#     };
-#   };
+      env.RUST_SRC_PATH = cfg.rust-src;
 
-#   config = lib.mkMerge [
-#     (lib.mkIf cfg.enable {
-#       packages = [ cfg.package ] ++ lib.optional pkgs.stdenv.isDarwin pkgs.libiconv;
+      # enable compiler tooling by default to expose things like cc
+      languages.c.enable = lib.mkDefault true;
+    })
+    (lib.mkIf (cfg.toolchain != null) {
+      languages.rust.package = lib.mkForce
+        (cfg.toolchain.withComponents cfg.components);
 
-#       # enable compiler tooling by default to expose things like cc
-#       languages.c.enable = lib.mkDefault true;
-
-#       pre-commit.tools.cargo = tryPath "${cfg.package}/bin/cargo";
-#       pre-commit.tools.rustfmt = tryPath "${cfg.package}/bin/rustfmt";
-#       pre-commit.tools.clippy = tryPath "${cfg.package}/bin/clippy";
-#     })
-#     (lib.mkIf (cfg.enable && pkgs.stdenv.isDarwin) {
-#       env.RUSTFLAGS = [ "-L framework=${config.env.DEVENV_PROFILE}/Library/Frameworks" ];
-#       env.RUSTDOCFLAGS = [ "-L framework=${config.env.DEVENV_PROFILE}/Library/Frameworks" ];
-#       env.CFLAGS = [ "-iframework ${config.env.DEVENV_PROFILE}/Library/Frameworks" ];
-#     })
-#     (lib.mkIf (cfg.toolchain != null && cfg.version != null)
-#       throw "cannot specify both languages.rust.toolchain and languages.rust.version"
-#     )
-#     (lib.mkIf (cfg.toolchain != null && cfg.components != null) {
-#       languages.rust.package = cfg.toolchain.withComponents cfg.components;
-#     })
-#   ];
-# }
+      languages.rust.rust-src = lib.mkForce "${cfg.toolchain.rust-src}/lib/rustlib/src/rust/library";
+    })
+    (lib.mkIf (cfg.version != null) {
+      languages.rust.toolchain = lib.mkForce (fenix.${cfg.version});
+    })
+  ];
+}
