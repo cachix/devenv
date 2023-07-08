29a3d42f75f794da2b65ab8a71a05c8c198d3aad
Chris Cummings <chris@thesogu.com>
2023-07-08 17:43:38 -0500

    Add an example hello world rust app and test it.

--- examples/rust/.test.sh
+++ examples/rust/.test.sh
@@ -3,10 +3,13 @@ set -ex
 cargo --version
 rustc --version
 
-echo "$RUSTFLAGS" | grep -- "--jobs 1"
+echo "$RUSTFLAGS" | grep -- "--verbose"
 
-if [[ "$(uname)" -eq "Darwin" ]] then
-  echo "$RUSTFLAGS" | grep -- "-L framework=$DEVENV_PROFILE/Library/Frameworks --jobs 1"
+if [[ "$(uname)" == "Darwin" ]] then
+  echo "$RUSTFLAGS" | grep -- "-L framework=$DEVENV_PROFILE/Library/Frameworks --verbose"
   echo "$RUSTDOCFLAGS" | grep -- "-L framework=$DEVENV_PROFILE/Library/Frameworks"
   echo "$CFLAGS" | grep -- "-iframework $DEVENV_PROFILE/Library/Frameworks"
 fi
+
+cd app
+cargo run
