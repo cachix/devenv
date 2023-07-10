e175a9f9262546e96276c5537f0a51036dec74f7
Domen Kožar <domen@dev.si>
2023-07-10 11:27:33 +0100

    Rewrite CLI in Python

--- flake.nix
+++ flake.nix
@@ -26,6 +26,10 @@
     url = "github:nix-community/poetry2nix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
+  inputs.poetry2nix = {
+    url = "github:nix-community/poetry2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs = { self, nixpkgs, pre-commit-hooks, nix, ... }@inputs:
     let
