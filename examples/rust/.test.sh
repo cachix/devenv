afd4ba1b0480dcee0d47bcf45328236d7fcb6692
Chris Cummings <chris@thesogu.com>
2023-07-10 10:28:27 -0500

    Update examples/rust/.test.sh
    Co-authored-by: Domen Kožar <domen@enlambda.com>

--- examples/rust/.test.sh
+++ examples/rust/.test.sh
@@ -3,7 +3,7 @@ set -ex
 cargo --version
 rustc --version
 
-if [[ "$(uname)" == "Darwin" ]] then
+if [[ "$(uname)" == "Darwin" ]]; then
   echo "$RUSTFLAGS" | grep -- "-L framework=$DEVENV_PROFILE/Library/Frameworks"
   echo "$RUSTDOCFLAGS" | grep -- "-L framework=$DEVENV_PROFILE/Library/Frameworks"
   echo "$CFLAGS" | grep -- "-iframework $DEVENV_PROFILE/Library/Frameworks"
