74dc714e3adde601734144734d2cb14d2594abef
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-07-10 23:32:35 -0500

    Refactor wrapper options as submodules

--- src/modules/integrations/aws-vault.nix
+++ src/modules/integrations/aws-vault.nix
@@ -21,34 +21,58 @@ in
       '';
     };
 
-    awscli2WrapperEnable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = ''
-        Wraps awscli2 binary as `aws-vault exec <profile> -- aws <args>`.
-      '';
+    awscli2Wrapper = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          enable = lib.mkEnableOption ''
+            Wraps awscli2 binary as `aws-vault exec <profile> -- aws <args>`.
+          '';
+
+          package = lib.mkOption {
+            type = lib.types.package;
+            default = pkgs.awscli2;
+            defaultText = lib.literalExpression "pkgs.awscli2";
+            description = "The awscli2 package to use.";
+          };
+        };
+      };
+      defaultText = lib.literalExpression "pkgs";
+      default = { };
+      description = "Attribute set of packages including awscli2";
     };
 
-    terraformWrapperEnable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = ''
-        Wraps terraform binary as `aws-vault exec <profile> -- terraform <args>`.
-      '';
+    terraformWrapper = lib.mkOption {
+      type = lib.types.submodule {
+        options = {
+          enable = lib.mkEnableOption ''
+            Wraps terraform binary as `aws-vault exec <profile> -- terraform <args>`.
+          '';
+
+          package = lib.mkOption {
+            type = lib.types.package;
+            default = pkgs.terraform;
+            defaultText = lib.literalExpression "pkgs.terraform";
+            description = "The terraform package to use.";
+          };
+        };
+      };
+      defaultText = lib.literalExpression "pkgs";
+      default = { };
+      description = "Attribute set of packages including terraform";
     };
   };
 
   config = lib.mkMerge [
-    (lib.mkIf (cfg.enable && cfg.awscli2WrapperEnable) {
+    (lib.mkIf (cfg.enable && cfg.awscli2Wrapper.enable) {
       packages = [
         (pkgs.writeScriptBin "aws" ''
-          ${cfg.package}/bin/aws-vault exec ${cfg.profile} -- ${pkgs.awscli2}/bin/aws "$@"
+          ${cfg.package}/bin/aws-vault exec ${cfg.profile} -- ${cfg.awscli2Wrapper.package}/bin/aws "$@"
         '')
       ];
     })
-    (lib.mkIf (cfg.enable && cfg.terraformWrapperEnable) {
+    (lib.mkIf (cfg.enable && cfg.terraformWrapper.enable) {
       languages.terraform.package = pkgs.writeScriptBin "terraform" ''
-        ${cfg.package}/bin/aws-vault exec ${cfg.profile} -- ${pkgs.terraform}/bin/terraform "$@"
+        ${cfg.package}/bin/aws-vault exec ${cfg.profile} -- ${cfg.terraformWrapper.package}/bin/terraform "$@"
       '';
     })
   ];
