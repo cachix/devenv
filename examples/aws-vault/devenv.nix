68fcf9337b170d250df81f3f632b858423f3304d
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2023-07-10 23:33:42 -0500

    Update example

--- examples/aws-vault/devenv.nix
+++ examples/aws-vault/devenv.nix
@@ -4,7 +4,7 @@
   aws-vault = {
     enable = true;
     profile = "aws-profile";
-    awscli2WrapperEnable = true;
-    terraformWrapperEnable = true;
+    awscli2Wrapper.enable = true;
+    terraformWrapper.enable = true;
   };
 }
