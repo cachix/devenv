c24eaf289474bd2fb1531f64dd7e631546764962
Yang, Bo <yang-bo@yang-bo.com>
2023-07-20 16:34:09 +0000

    Avoid more builtins.pathExists

--- src/modules/integrations/dotenv.nix
+++ src/modules/integrations/dotenv.nix
@@ -4,17 +4,6 @@ let
   cfg = config.dotenv;
 
   dotenvPath = config.devenv.root + "/" + cfg.filename;
-
-  parseLine = line:
-    let
-      parts = builtins.match "(.+) *= *(.+)" line;
-    in
-    if (!builtins.isNull parts) && (builtins.length parts) == 2 then
-      { name = builtins.elemAt parts 0; value = builtins.elemAt parts 1; }
-    else
-      null;
-
-  parseEnvFile = content: builtins.listToAttrs (lib.filter (x: !builtins.isNull x) (map parseLine (lib.splitString "\n" content)));
 in
 {
   options.dotenv = {
@@ -28,11 +17,6 @@ in
       '';
     };
 
-    resolved = lib.mkOption {
-      type = lib.types.attrsOf lib.types.anything;
-      internal = true;
-    };
-
     disableHint = lib.mkOption {
       type = lib.types.bool;
       default = false;
@@ -43,27 +27,33 @@ in
   };
 
   config = lib.mkMerge [
-    (lib.mkIf (cfg.enable && builtins.pathExists dotenvPath) {
-      env = lib.mapAttrs (name: value: lib.mkDefault value) config.dotenv.resolved;
-      dotenv.resolved = parseEnvFile (builtins.readFile dotenvPath);
+    (lib.mkIf cfg.enable {
+      enterShell = ''
+        if test -f ${lib.escapeShellArg dotenvPath}
+        then
+          export $(xargs < ${lib.escapeShellArg dotenvPath})
+        fi
+      '';
     })
-    (lib.mkIf (cfg.enable && !builtins.pathExists dotenvPath) (
-      let
-        exampleExists = builtins.pathExists (dotenvPath + ".example");
-      in
+    (lib.mkIf cfg.enable (
       {
         enterShell = ''
-          echo "💡 A ${cfg.filename} file was not found, while dotenv integration is enabled."
-          echo 
-          ${lib.optionalString exampleExists ''
-            echo "   To create .env, you can copy the example file:"
+          # Test if the file exists, and if not, print a hint.
+          if ! test -f ${lib.escapeShellArg dotenvPath}
+          then
+            echo "💡 A ${cfg.filename} file was not found, while dotenv integration is enabled."
+            echo 
+            if test -f ${lib.escapeShellArg "${dotenvPath}.example}"}
+            then
+              echo "   To create .env, you can copy the example file:"
+              echo
+              echo "   $ cp ${dotenvPath}.example ${dotenvPath}";
+              echo
+            fi
+            echo "   To disable it, add \`dotenv.enable = false;\` to your devenv.nix file.";
             echo
-            echo "   $ cp ${dotenvPath}.example ${dotenvPath}";
-            echo
-          ''}
-          echo "   To disable it, add \`dotenv.enable = false;\` to your devenv.nix file.";
-          echo
-          echo "See https://devenv.sh/integrations/dotenv/ for more information.";
+            echo "See https://devenv.sh/integrations/dotenv/ for more information.";
+          fi
         '';
       }
     ))
