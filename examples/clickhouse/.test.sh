1d37fdb0d13c36be996d68ee06bed5bde8f39083
Domen Kožar <domen@dev.si>
2023-08-19 09:38:53 +0100

    shellcheck

--- examples/clickhouse/.test.sh
+++ examples/clickhouse/.test.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -xe 
 timeout 20 bash -c 'until echo > /dev/tcp/localhost/9000; do sleep 0.5; done'
 sleep 2
--- examples/postgres/.test.sh
+++ examples/postgres/.test.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 set -ex
 timeout 20 bash -c 'until psql -h /tmp -c "SELECT 1" mydb 2>/dev/null; do sleep 0.5; done'
