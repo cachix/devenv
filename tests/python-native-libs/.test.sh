94730a1a71bf3aa6fa766a034a793e60a4491daf
Domen Kožar <domen@dev.si>
2023-08-21 21:33:52 +0100

    Add languages.python.manylinux.enable, pass libraries to the interpreter.

--- tests/python-native-libs/.test.sh
+++ tests/python-native-libs/.test.sh
@@ -5,3 +5,5 @@ python -c "from PIL import Image"
 python -c "import grpc_tools.protoc"
 python -c "import transformers"
 python -c "import torch"
+
+# TODO: invoke a subprocess with an old glibc and assert it doesn't crash
