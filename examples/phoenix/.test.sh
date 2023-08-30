97336c7b020055f7270e3add4e701e2f72ca7555
Arvin Sevilla <sevillaarvin@gmail.com>
2023-08-30 22:13:15 +0800

    docs(phoenix): add example for elixir phoenix

---
+++ examples/phoenix/.test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ex
+mix local.hex --force
+echo Y | mix archive.install hex phx_new
+echo Y | mix phx.new hello
+awk -i inplace '{
+  gsub(/username: "postgres",/, "username: \"" ENVIRON["USER"] "\",")
+  gsub(/password: "postgres",/, "password: \"\",")
+  print
+}' 'hello/config/dev.exs'
+devenv up&
+timeout 20 bash -c 'until echo > /dev/tcp/localhost/4000; do sleep 0.5; done'
+curl -s http://localhost:4000/ | grep "Phoenix Framework"
---
+++ examples/phoenix/devenv.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  packages = with pkgs;
+  [ inotify-tools
+  ];
+
+  languages.elixir.enable = true;
+
+  services.postgres = {
+    enable = true;
+    listen_addresses = "127.0.0.1";
+  };
+
+  processes.phoenix.exec = "cd hello && mix ecto.create && mix phx.server";
+}
