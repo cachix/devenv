8542d7754b5cbb89d36bc2c983c66413bb339f84
Arvin Sevilla <sevillaarvin@gmail.com>
2023-08-31 22:07:10 +0800

    fix(phoenix): make in-place editing compatible with macos and linux
    https://stackoverflow.com/a/22084103

--- examples/phoenix/.test.sh
+++ examples/phoenix/.test.sh
@@ -4,11 +4,10 @@ mix local.hex --force
 mix local.rebar --force
 echo Y | mix archive.install hex phx_new
 echo Y | mix phx.new hello
-awk -i inplace '{
-  gsub(/username: "postgres",/, "username: \"" ENVIRON["USER"] "\",")
-  gsub(/password: "postgres",/, "password: \"\",")
-  print
-}' 'hello/config/dev.exs'
+sed -i.bak \
+    -e "s/username: \"postgres\",/username: \"$USER\",/" \
+    -e "s/password: \"postgres\",/password: \"\",/" \
+    ./hello/config/dev.exs && rm ./hello/config/dev.exs.bak
 devenv up&
 timeout 20 bash -c 'until echo > /dev/tcp/localhost/4000; do sleep 0.5; done'
 curl -s http://localhost:4000/ | grep "Phoenix Framework"
