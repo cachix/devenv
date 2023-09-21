cc111238dc71dea889ccf888962178a16addbac8
Sander <hey@sandydoo.me>
2023-09-21 22:36:09 +0400

    poetry run

--- build_cloudflare.sh
+++ build_cloudflare.sh
@@ -2,4 +2,4 @@ set -xe
 pip install poetry
 poetry install
 pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.1.5-insiders-4.32.4
-mkdocs build --config-file mkdocs.insiders.yml
+poetry run -- mkdocs build --config-file mkdocs.insiders.yml
