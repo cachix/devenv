d0ab36881f5a72b2c213634318736e317ec8c6e1
Sander <hey@sandydoo.me>
2023-09-21 23:07:57 +0400

    Update mkdocs

--- build_cloudflare.sh
+++ build_cloudflare.sh
@@ -1,4 +1,4 @@
 set -xe
 poetry install
-pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.1.5-insiders-4.32.4
+pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.4.0-insiders-4.42.0
 mkdocs build --config-file mkdocs.insiders.yml
