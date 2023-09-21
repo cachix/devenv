361c767be9bd5a28f401ba8ddebc680807afb915
Sander <hey@sandydoo.me>
2023-09-21 21:04:31 +0000

    Update CF build script

--- build_cloudflare.sh
+++ build_cloudflare.sh
@@ -1,5 +1,3 @@
-set -xe 
-pip install poetry
-poetry install
+set -xe
 pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.4.0-insiders-4.42.0
-mkdocs build --config-file mkdocs.insiders.yml
+poetry run -- mkdocs build --config-file mkdocs.insiders.yml
