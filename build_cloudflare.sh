5e562a066d26a89c0a89163196c8f1f331e32a5f
Sander <hey@sandydoo.me>
2023-09-21 22:54:18 +0400

    ?

--- build_cloudflare.sh
+++ build_cloudflare.sh
@@ -1,5 +1,4 @@
-set -xe 
-pip install poetry
+set -xe
 poetry install
 pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.1.5-insiders-4.32.4
-poetry run -- mkdocs build --config-file mkdocs.insiders.yml
+mkdocs build --config-file mkdocs.insiders.yml
