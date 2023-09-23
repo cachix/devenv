cb54e540a85c5e029b175e2b8d0de29afb704192
Domen Kožar <domen@dev.si>
2023-09-23 15:27:13 +0100

    remove mkdocs insiders now that blog is part of community

--- build_cloudflare.sh
+++ build_cloudflare.sh
@@ -1,3 +1,3 @@
 set -xe
-pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.4.0-insiders-4.42.0
-poetry run -- mkdocs build --config-file mkdocs.insiders.yml
+poetry install
+poetry run -- mkdocs build
