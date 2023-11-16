157d98c44e1818454da7c539571ab106050adc8b
Oscar Izquierdo <25722135+oscar-izval@users.noreply.github.com>
2023-11-16 01:40:37 +0100

    Add integration with nixpkgs-terraform

--- src/modules/languages/terraform.nix
+++ src/modules/languages/terraform.nix
@@ -1,7 +1,15 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, lib, inputs, ... }:
 
 let
   cfg = config.languages.terraform;
+
+  nixpkgs-terraform = inputs.nixpkgs-terraform or (throw ''
+    To use languages.terraform.version, you need to add the following to your devenv.yaml:
+
+      inputs:
+        nixpkgs-terraform:
+          url: github:stackbuilders/nixpkgs-terraform
+  '');
 in
 {
   options.languages.terraform = {
@@ -13,9 +21,23 @@ in
       defaultText = lib.literalExpression "pkgs.terraform";
       description = "The Terraform package to use.";
     };
+
+    version = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = ''
+        The Terraform version to use.
+        This automatically sets the `languages.terraform.package` using [nixpkgs-terraform](https://github.com/stackbuilders/nixpkgs-terraform).
+      '';
+      example = "1.5.0 or 1.6.2";
+    };
   };
 
   config = lib.mkIf cfg.enable {
+    languages.terraform.package = lib.mkMerge [
+      (lib.mkIf (cfg.version != null) (nixpkgs-terraform.packages.${pkgs.stdenv.system}.${cfg.version} or (throw "Unsupported Terraform version, see https://github.com/stackbuilders/nixpkgs-terraform#available-versions")))
+    ];
+
     packages = with pkgs; [
       cfg.package
       terraform-ls
