0794e17528c2fb202c7c24a735f62d6653cea16f
Manuel Mendez <github@i.m.mmlb.dev>
2023-12-04 22:41:12 -0500

    Add description to script
    Just some metadata that can be useful in various contexts like
    enterShell for example.

--- examples/scripts/devenv.nix
+++ examples/scripts/devenv.nix
@@ -1,7 +1,21 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  scripts."gitversion".exec = ''
-    echo hello $(${pkgs.git}/bin/git --version)
+  packages = [ pkgs.curl pkgs.jq ];
+
+  scripts.silly-example.exec = ''curl "https://httpbin.org/get?$1" | jq .args'';
+  scripts.silly-example.description = "curls httpbin with provided arg";
+
+  scripts.serious-example.exec = ''${pkgs.cowsay}/bin/cowsay "$*"'';
+  scripts.serious-example.description = ''echoes args in a very serious manner'';
+
+  enterShell = ''
+    echo
+    echo 🦾 Helper scripts you can run to make your development richer:
+    echo 🦾
+    ${pkgs.gnused}/bin/sed -e 's| |••|g' -e 's|=| |' <<EOF | ${pkgs.util-linuxMinimal}/bin/column -t | ${pkgs.gnused}/bin/sed -e 's|^|🦾 |' -e 's|••| |g'
+    ${lib.generators.toKeyValue {} (lib.mapAttrs (name: value: value.description) config.scripts)}
+    EOF
+    echo
   '';
 }
