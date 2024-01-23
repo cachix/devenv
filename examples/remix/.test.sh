2a1e7e23dab9e5d2d33a9f06ee20cf88f7a25d97
Fatih Altinok <fatihaltinok@live.com>
2024-01-23 14:28:19 +0300

    Clean up `create-remix` output

--- examples/remix/.test.sh
+++ examples/remix/.test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
-npm create remix -- . --template=frontsideair/remix-prisma-postgresql-template --no-install --no-git-init --overwrite
+npm create remix -- . --template=frontsideair/remix-prisma-postgresql-template --no-install --no-git-init --no-motion --overwrite
 devenv up&
 timeout 20 bash -c 'until echo > /dev/tcp/localhost/5100; do sleep 0.5; done'
 npx prisma migrate dev && npx prisma db seed
--- examples/remix/README.md
+++ examples/remix/README.md
@@ -1,8 +1,7 @@
 ```shell-session
-$ npm create remix -- . --template=frontsideair/remix-prisma-postgresql-template --no-install --no-git-init --overwrite
+$ npm create remix -- . --template=frontsideair/remix-prisma-postgresql-template --no-install --no-git-init --no-motion --overwrite
 $ devenv shell
 $ devenv up
 $ npx prisma migrate dev
 $ npx prisma db seed
 ```
-
