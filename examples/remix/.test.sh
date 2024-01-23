edfcc4c2fcb6aac54c79da532ef0da6245b62155
Fatih Altinok <fatihaltinok@live.com>
2024-01-23 09:06:06 +0300

    Create remix nodejs+postgres example

---
+++ examples/remix/.test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -ex
+npm create remix -- . --template=frontsideair/remix-prisma-postgresql-template --no-install --no-git-init --overwrite
+devenv up&
+timeout 20 bash -c 'until echo > /dev/tcp/localhost/5100; do sleep 0.5; done'
+npx prisma migrate dev && npx prisma db seed
+curl -s http://localhost:5100/ | grep "hello"
