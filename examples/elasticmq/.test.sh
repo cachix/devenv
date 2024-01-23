e7211866357791f14d57e774584f5a8beee9ce1f
Chris McDonough <chrism@plope.com>
2024-01-22 20:15:52 -0500

    make tests pass

--- examples/elasticmq/.test.sh
+++ examples/elasticmq/.test.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-devenv up &
-DEVENV_PID=$!
-export DEVENV_PID
-
-function stop() {
-    pkill -P "$DEVENV_PID"
-}
-
-trap stop EXIT
-
 timeout 60 bash -c 'until echo > /dev/tcp/localhost/9325; do sleep 0.5; done'
 
 QUEUE_NAME=$(curl http://localhost:9325/statistics/queues -s | jq .[].name -r)
--- examples/influxdb/.test.sh
+++ examples/influxdb/.test.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-devenv up &
-DEVENV_PID=$!
-export DEVENV_PID
-
-function stop() {
-    pkill -P "$DEVENV_PID"
-}
-
-trap stop EXIT
-
 # We test for the none-default port, configured in the nix file
 timeout 60 bash -c 'until echo > /dev/tcp/localhost/8087; do sleep 0.5; done'
 
