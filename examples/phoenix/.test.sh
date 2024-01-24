da4589b7b11962e71efb9e438d727cb63f81f744
Chris McDonough <chrism@plope.com>
2024-01-23 19:51:49 -0500

    mark phoenix as broken

--- examples/phoenix/.test.sh
+++ examples/phoenix/.test.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -ex
-mkdir hello
-mix local.hex --force
-mix local.rebar --force
-echo Y | mix archive.install hex phx_new
-echo Y | mix phx.new hello
-sed -i.bak -e "s/username: \"postgres\",/socket_dir: System.get_env(\"PGDATA\"),/" \
-    ./hello/config/dev.exs && rm ./hello/config/dev.exs.bak
-devenv up&
-timeout 20 bash -c 'until echo > /dev/tcp/localhost/4000; do sleep 0.5; done'
-curl -s http://localhost:4000/ | grep "Phoenix Framework"
+# mix local.hex --force
+# mix local.rebar --force
+# echo Y | mix archive.install hex phx_new
+# echo Y | mix phx.new hello
+# sed -i.bak -e "s/username: \"postgres\",/socket_dir: System.get_env(\"PGDATA\"),/" \
+#     ./hello/config/dev.exs && rm ./hello/config/dev.exs.bak
+# devenv up&
+# timeout 20 bash -c 'until echo > /dev/tcp/localhost/4000; do sleep 0.5; done'
+# curl -s http://localhost:4000/ | grep "Phoenix Framework"
+echo "this example is currently broken"
+exit 2
