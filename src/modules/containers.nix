f6e3446ddc4a41f7d00e22467a8b3b7fa9035726
Chris McDonough <chrism@plope.com>
2024-01-28 16:41:40 -0500

    work towards getting nonroot containers; currently works except devenv doesnt get installed and we ignore the copyToRoot option

--- src/modules/containers.nix
+++ src/modules/containers.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, lib, self, ... }:
+{ pkgs, config, lib, self, devenv, ... }:
 
 let
   projectName = name:
@@ -31,31 +31,99 @@ let
     # expand any envvars before exec
     cmd="`echo "$@"|${pkgs.envsubst}/bin/envsubst`"
 
-    exec $cmd
+    bash -c "$cmd"
   '';
+  user = "user";
+  group = "user";
+  uid = "1000";
+  gid = "1000";
+  homedir = "/env";
+
+  mkTemp = (pkgs.runCommand "tmp" { } ''
+    mkdir -p $out/tmp
+  '');
+
+  mkUser = (pkgs.runCommand "user" { } ''
+    mkdir -p $out${homedir}
+    cp -R ${self}/* $out${homedir}/
+
+    mkdir -p $out/etc/pam.d
+
+    echo "root:x:0:0:System administrator:/root:/bin/bash" > \
+          $out/etc/passwd
+    echo "${user}:x:${uid}:${gid}::${homedir}:/bin/bash" >> \
+          $out/etc/passwd
+
+    echo "root:!x:::::::" > $out/etc/shadow
+    echo "${user}:!x:::::::" >> $out/etc/shadow
+
+    echo "root:x:0:" > $out/etc/group
+    echo "${group}:x:${gid}:" >> $out/etc/group
+
+    cat > $out/etc/pam.d/other <<EOF
+    account sufficient pam_unix.so
+    auth sufficient pam_rootok.so
+    password requisite pam_unix.so nullok sha512
+    session required pam_unix.so
+    EOF
+
+    touch $out/etc/login.defs
+  '');
+
   mkDerivation = cfg: nix2container.nix2container.buildImage {
     name = cfg.name;
     tag = cfg.version;
+    initializeNixDatabase = true;
+    nixUid = lib.toInt uid;
+    nixGid = lib.toInt gid;
+
     copyToRoot = [
-      (pkgs.runCommand "create-paths" { } ''
-        mkdir -p $out/tmp
-      '')
       (pkgs.buildEnv {
         name = "root";
         paths = [
           pkgs.coreutils-full
-          pkgs.bash
-        ] ++ lib.optionals (cfg.copyToRoot != null)
-          (if builtins.typeOf cfg.copyToRoot == "list"
-          then cfg.copyToRoot
-          else [ cfg.copyToRoot ]);
-        pathsToLink = "/";
+          pkgs.bashInteractive
+          pkgs.su
+          pkgs.sudo
+          devenv
+        ];
+        pathsToLink = "/bin";
       })
+      mkUser
+      mkTemp
     ];
+
+    perms = [
+      {
+        path = mkUser;
+        regex = "${homedir}";
+        mode = "0744";
+        uid = lib.toInt uid;
+        gid = lib.toInt gid;
+        uname = user;
+        gname = group;
+      }
+      {
+        path = mkTemp;
+        regex = "/tmp";
+        mode = "1777";
+        uid = 0;
+        gid = 0;
+        uname = "root";
+        gname = "root";
+      }
+    ];
+
     config = {
-      Env = lib.mapAttrsToList (name: value: "${name}=${lib.escapeShellArg (toString value)}") config.env;
-      Cmd = [ cfg.startupCommand ];
       Entrypoint = cfg.entrypoint;
+      User = "${user}";
+      WorkingDir = "${homedir}";
+      Env = lib.mapAttrsToList
+        (name: value:
+          "${name}=${lib.escapeShellArg (toString value)}"
+        )
+        config.env ++ [ "HOME=${homedir}" "USER=${user}" ];
+      Cmd = [ cfg.startupCommand ];
     };
   };
 
@@ -201,7 +269,7 @@ in
       containers.${envContainerName}.isBuilding = true;
     })
     (lib.mkIf config.container.isBuilding {
-      devenv.root = lib.mkForce "/";
+      devenv.root = lib.mkForce "${homedir}";
     })
   ];
 }
