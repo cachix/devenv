7cabf2bb94625f768df8e56bd98c980ce4548819
Matthias Thym <git@thym.at>
2024-03-04 14:59:37 +0100

    Add support for specifying JavaScript root directory

--- src/modules/languages/javascript.nix
+++ src/modules/languages/javascript.nix
@@ -3,7 +3,7 @@
 let
   cfg = config.languages.javascript;
 
-  nodeModulesPath = "node_modules";
+  nodeModulesPath = "${lib.optionalString (cfg.directory != config.devenv.root) ''"${cfg.directory}/"''}node_modules";
 
   initNpmScript = pkgs.writeShellScript "init-npm.sh" ''
     function _devenv-npm-install()
@@ -11,7 +11,7 @@ let
       # Avoid running "npm install" for every shell.
       # Only run it when the "package-lock.json" file or nodejs version has changed.
       # We do this by storing the nodejs version and a hash of "package-lock.json" in node_modules.
-      local ACTUAL_NPM_CHECKSUM="${cfg.package.version}:$(${pkgs.nix}/bin/nix-hash --type sha256 package-lock.json)"
+      local ACTUAL_NPM_CHECKSUM="${cfg.package.version}:$(${pkgs.nix}/bin/nix-hash --type sha256 ${lib.optionalString (cfg.directory != config.devenv.root) ''"${cfg.directory}/"''}package-lock.json)"
       local NPM_CHECKSUM_FILE="${nodeModulesPath}/package-lock.json.checksum"
       if [ -f "$NPM_CHECKSUM_FILE" ]
         then
@@ -22,18 +22,18 @@ let
 
       if [ "$ACTUAL_NPM_CHECKSUM" != "$EXPECTED_NPM_CHECKSUM" ]
       then
-        if ${cfg.package}/bin/npm install
+        if ${cfg.package}/bin/npm install ${lib.optionalString (cfg.directory != config.devenv.root) "--prefix ${cfg.directory}"}
         then
           echo "$ACTUAL_NPM_CHECKSUM" > "$NPM_CHECKSUM_FILE"
         else
-          echo "Npm install failed. Run 'npm install' manually."
+          echo "Install failed. Run 'npm install' manually."
         fi
       fi
     }
 
-    if [ ! -f package.json ]
+    if [ ! -f ${lib.optionalString (cfg.directory != config.devenv.root) ''"${cfg.directory}/"''}package.json ]
     then
-      echo "No package.json found. Run 'npm init' to create one." >&2
+      echo "No package.json found${lib.optionalString (cfg.directory != config.devenv.root) ''"in ${cfg.directory}"''}. Run '${lib.optionalString (cfg.directory != config.devenv.root) ''"cd ${cfg.directory}/ && "''}npm init' to create one." >&2
     else
       _devenv-npm-install
     fi
@@ -43,11 +43,23 @@ in
   options.languages.javascript = {
     enable = lib.mkEnableOption "tools for JavaScript development";
 
+    directory = lib.mkOption {
+      type = lib.types.str;
+      default = config.devenv.root;
+      defaultText = lib.literalExpression "config.devenv.root";
+      description = ''
+        The JavaScript project's root directory. Defaults to the root of the devenv project.
+        Can be an absolute path or one relative to the root of the devenv project.
+      '';
+      example = "./directory";
+    };
+
     package = lib.mkOption {
       type = lib.types.package;
       default = pkgs.nodejs;
       defaultText = lib.literalExpression "pkgs.nodejs";
       description = "The Node package to use.";
+      example = "pkgs.bun";
     };
 
     corepack = {
@@ -74,3 +86,7 @@ in
     );
   };
 }
+
+
+
+
