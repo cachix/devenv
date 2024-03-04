0fb075217b628d29f32806226dd84a9a64953bd8
Matthias Thym <git@thym.at>
2024-03-04 12:56:26 +0100

    Add OpenTofu and use as default for aws-vault

--- examples/aws-vault/devenv.nix
+++ examples/aws-vault/devenv.nix
@@ -1,10 +1,14 @@
-{ pkgs, ... }: {
-  languages.terraform.enable = true;
+{ pkgs, ... }:
+
+{
+  # Since Terraform adopted a non-free license (BSL 1.1) in August 2023,
+  # using terraform instead of opentofu now requires adding `allowUnfree: true` to `devenv.yaml`
+  languages.opentofu.enable = true;
 
   aws-vault = {
     enable = true;
     profile = "aws-profile";
     awscliWrapper.enable = true;
-    terraformWrapper.enable = true;
+    opentofuWrapper.enable = true;
   };
 }
