7f39eef26a6fc0b3ac6f2054345efe57fc5abe64
Domen Kožar <domen@dev.si>
2024-03-12 10:43:08 +0000

    Introduce devenv-nixpkgs
    Battle-tested nixpkgs using devenv's extensive testing infrastructure.

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
   inputs.pre-commit-hooks = {
     url = "github:cachix/pre-commit-hooks.nix";
     inputs = {
