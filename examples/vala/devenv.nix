bd8a9cc7ce6ded0da441c2a1a4fcb21fdda26eba
Domen Kožar <domen@dev.si>
2024-03-17 06:39:05 +0000

    fix a few more tests

--- examples/vala/devenv.nix
+++ examples/vala/devenv.nix
@@ -8,7 +8,7 @@
 
   languages = {
     vala = {
-      enable = true;
+      enable = pkgs.stdenv.isLinux;
       # This is the default package for Vala for the configured channel (see nixpkgs input in devenv.yaml)
       # It can be configured to use a specific version
       # Take a look [here](https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vala) to find out which versions are available
