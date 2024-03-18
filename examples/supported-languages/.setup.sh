27f89238a3c2a7cc716b0323f19686446df700c8
Domen Kožar <domen@dev.si>
2024-03-18 21:30:33 +0000

    disable purescript on aarch64-linux

--- examples/supported-languages/.setup.sh
+++ examples/supported-languages/.setup.sh
@@ -2,4 +2,6 @@
 echo "{ pkgs, lib, ... }: {"  > devenv.local.nix
 echo "  languages.unison.enable = lib.mkForce (!(pkgs.stdenv.isLinux && pkgs.stdenv.isAarch64));" >> devenv.local.nix
 echo "  languages.standardml.enable = lib.mkForce (!pkgs.stdenv.isAarch64);" >> devenv.local.nix
+# https://github.com/NixOS/nixpkgs/issues/297019
+echo "  languages.purescript.enable = lib.mkForce (!pkgs.stdenv.isAarch64);" >> devenv.local.nix
 echo "}" >> devenv.local.nix
