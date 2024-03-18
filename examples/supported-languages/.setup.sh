b5681ea985f503ee352440dbd04af5b0b6af80b3
Domen Kožar <domen@dev.si>
2024-03-18 12:11:01 +0000

    fix supported-languages example

--- examples/supported-languages/.setup.sh
+++ examples/supported-languages/.setup.sh
@@ -1,5 +1,5 @@
 # arm is not supported yet
 echo "{ pkgs, lib, ... }: {"  > devenv.local.nix
-echo "languages.unison.enable = lib.mkForce (!(pkgs.stdenv.isLinux && pkgs.stdenv.isAarch64)); }" > devenv.local.nix
-echo "languages.standardml.enable = lib.mkForce (!pkgs.stdenv.isAarch64); }" > devenv.local.nix
+echo "  languages.unison.enable = lib.mkForce (!(pkgs.stdenv.isLinux && pkgs.stdenv.isAarch64));" >> devenv.local.nix
+echo "  languages.standardml.enable = lib.mkForce (!pkgs.stdenv.isAarch64);" >> devenv.local.nix
 echo "}" >> devenv.local.nix
