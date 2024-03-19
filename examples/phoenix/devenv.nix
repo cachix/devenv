89f61448a203c8c083782134f2eea6d0fad9586e
Arvin Sevilla <sevillaarvin@gmail.com>
2024-03-19 15:49:10 +0800

    docs(phoenix): add example for elixir phoenix
    patterned using example/rubyonrails

---
+++ examples/phoenix/devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, ... }:
+
+{
+  packages = lib.optionals pkgs.stdenv.isLinux [ pkgs.inotify-tools ];
+
+  languages.elixir.enable = true;
+
+  services.postgres = {
+    enable = true;
+    initialScript = ''
+      CREATE ROLE postgres WITH LOGIN PASSWORD 'postgres' SUPERUSER;
+    '';
+  };
+
+  processes.phoenix.exec = "cd hello && mix phx.server";
+
+  enterShell = ''
+    if [ ! -d "hello" ]; then
+      mix local.hex --force
+      mix local.rebar --force
+      mix archive.install hex phx_new
+      mix phx.new hello
+    fi
+  '';
+}
