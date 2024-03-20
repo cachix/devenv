883b0e4894541546b5d3988f79cb160e515de1ba
Domen Kožar <domen@dev.si>
2024-03-20 10:12:12 +0000

    fix flake template

--- templates/simple/flake.nix
+++ templates/simple/flake.nix
@@ -1,8 +1,9 @@
 {
   inputs = {
-    nixpkgs.url = "github:github:cachix/devenv-nixpkgs/rolling";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   nixConfig = {
