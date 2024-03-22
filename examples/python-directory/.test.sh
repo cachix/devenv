37cb967bc4ad26a177ca3fc6552ecf70b9ed53d6
Domen Kožar <domen@dev.si>
2024-03-22 09:58:17 +0000

    cleanup python directory

--- examples/python-directory/.test.sh
+++ examples/python-directory/.test.sh
@@ -2,9 +2,10 @@
 set -exu
 POETRY_VENV="$PWD/directory/.venv"
 [ -d "$POETRY_VENV" ]
-[ "$(poetry env info --path)" = "$POETRY_VENV" ]
 [ "$(command -v python)" = "$POETRY_VENV/bin/python" ]
 python --version
 poetry --version
-poetry run python -c 'import requests'
 python -c 'import requests'
+cd directory
+[ "$(poetry env info --path)" = "$POETRY_VENV" ]
+poetry run python -c 'import requests'
--- examples/python-poetry/devenv.nix
+++ examples/python-poetry/devenv.nix
@@ -1,16 +1,9 @@
-{ pkgs, config, ... }:
+{ pkgs, lib, config, ... }:
 
 {
   packages = [
     # A python dependency outside of poetry.
     config.languages.python.package.pkgs.pjsua2
-  ];
-
-  # this envvar can be removed and the lib can be moved into
-  # languages.python.libraries when we start working against env-venv
-  env.LD_LIBRARY_PATH = lib.makeLibraryPath [
-    # A native dependency of numpy
-    pkgs.zlib
   ];
 
   languages.python = {
