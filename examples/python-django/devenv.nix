b6e32b698a79d72fb6032a97c87d0fa409648621
Rowan Molony <rowanmolony@gmail.com>
2024-04-08 20:52:01 +0100

    docs: add django example
    Co-authored-by: bobvanderlinden
    Co-authored-by: domenkozar

---
+++ examples/python-django/devenv.nix
@@ -0,0 +1,43 @@
+{ pkgs, config, ... }:
+
+let
+  db_user = "postgres";
+  db_host = "localhost";
+  db_port = "5432";
+  db_name = "db";
+in
+{
+  packages = [ pkgs.git pkgs.postgresql_14 ];
+
+  languages.python = {
+    enable = true;
+    package = pkgs.python310;
+    poetry.enable = true;
+  };
+
+  env = {
+    DATABASE_URL = "postgres://${db_user}@/${db_name}?host=${config.env.PGHOST}";
+    DEBUG = true;
+    SECRET_KEY = "123";
+    STATIC_ROOT = "/tmp";
+  };
+
+  services.postgres = {
+    enable = true;
+    initialScript = "CREATE USER ${db_user} SUPERUSER;";
+    initialDatabases = [{ name = db_name; }];
+    listen_addresses = db_host;
+  };
+
+  processes = {
+    runserver.exec = ''
+      devenv shell python manage.py runserver
+    '';
+  };
+
+  enterTest = ''
+    wait_for_port ${db_port}
+    python manage.py test
+  '';
+
+}
---
+++ examples/python-django/myapp/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+
+class Test(TestCase):
+    def test(self):
+        self.assertEqual(1, 1)
