be71050d79969f30c47aab6525d51a46bd55bcda
164182 <camden.possinger@delta.com>
2024-04-09 14:49:18 -0400

    add uv test

---
+++ tests/python-native-libs-uv/devenv.nix
@@ -0,0 +1,14 @@
+{ pkgs, lib, ... }: {
+  packages = [ pkgs.cairo ];
+
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    venv.requirements = ''
+      pillow
+      grpcio-tools
+      transformers
+    '';
+    uv.enable = true;
+  };
+}
