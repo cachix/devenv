ab9f56db4c94e4595a103b14f748b9df7944870f
Arvin Sevilla <sevillaarvin@gmail.com>
2024-04-18 15:32:03 +0800

    docs(phoenix): connect to unix socket instead of tcp
    issue: https://github.com/cachix/devenv/issues/1088
    reference: https://github.com/cachix/devenv/pull/803/files

--- examples/phoenix/devenv.nix
+++ examples/phoenix/devenv.nix
@@ -18,11 +18,17 @@
   processes.phoenix.exec = "cd hello && mix phx.server";
 
   enterShell = ''
+    mix local.hex --force
+    mix local.rebar --force
+    mix archive.install --force hex phx_new
+
     if [ ! -d "hello" ]; then
-      mix local.hex --force
-      mix local.rebar --force
-      mix archive.install --force hex phx_new
-      mix phx.new --install hello
+      # guard against multiple invocation of "enterShell"
+      mkdir hello
+
+      echo y | mix phx.new --install hello
+      sed -i.bak -e "s/hostname: \"localhost\"/socket_dir: System.get_env(\"PGHOST\")/" \
+        ./hello/config/dev.exs && rm ./hello/config/dev.exs.bak
     fi
   '';
 }
