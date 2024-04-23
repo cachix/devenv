1415c5c5d01e739afc4ff30886d6c3a032b85e80
Sander <hey@sandydoo.me>
2024-04-23 18:27:44 +0400

    rust-wasm-cross: fix darwin and aarch64-linux builds

--- examples/rust-wasm-cross/devenv.nix
+++ examples/rust-wasm-cross/devenv.nix
@@ -11,16 +11,21 @@
     components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" "rust-std" ];
   };
 
-  # These break us
-  # pre-commit.hooks = {
-  #  rustfmt.enable = true;
-  #  clippy.enable = true;
-  # };
+  pre-commit.hooks = {
+    clippy.enable = true;
+    rustfmt.enable = true;
+  };
 
   packages = [
     pkgs.wasm-pack
+    pkgs.binaryen # use a newer version of wasm-opt
     pkgs.nodejs
   ] ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk; [
     frameworks.Security
   ]);
+
+  # macOS workaround:
+  # The linker on macOS doesn't like the frameworks option when compiling to wasm32.
+  # See https://github.com/rust-lang/rust/issues/122333
+  env.RUSTFLAGS = lib.mkIf pkgs.stdenv.isDarwin (lib.mkForce "");
 }
