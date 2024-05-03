f9893f7242da322324e3f5727358e5035fd42408
Sander <hey@sandydoo.me>
2024-05-03 23:24:48 +0400

    fixup

--- src/modules/integrations/direnv.nix
+++ src/modules/integrations/direnv.nix
@@ -8,10 +8,10 @@ in
   options.direnv = {
     watchFiles = lib.mkOption {
       description = lib.mdDoc "A list of files to watch for changes and then reload the direnv environment";
-      # TODO: best type here? any way to exclude defaults?
       # TODO: regex support?
       type = types.listOf types.str;
       default = [ ];
+      example = [ "requirements.txt" ];
     };
 
     watchfiles = lib.mkOption {
