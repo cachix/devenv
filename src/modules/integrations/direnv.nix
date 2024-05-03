6c0d838f0d29fe4117cd695160996aed71077c79
Sander <hey@sandydoo.me>
2024-05-03 22:58:15 +0400

    fixup

--- src/modules/integrations/direnv.nix
+++ src/modules/integrations/direnv.nix
@@ -2,24 +2,23 @@
 
 let
   cfg = config.direnv;
+  inherit (lib) types;
 in
 {
   options.direnv = {
     watchFiles = lib.mkOption {
-      description = lib.mdDoc "Extra files to watch for changes and reload the direnv environment";
+      description = lib.mdDoc "A list of files to watch for changes and then reload the direnv environment";
       # TODO: best type here? any way to exclude defaults?
       # TODO: regex support?
-      type = lib.types.listOf lib.types.str;
+      type = types.listOf types.str;
       default = [ ];
     };
 
     watchfiles = lib.mkOption {
       internal = true;
-      type = lib.types.package;
+      type = types.package;
     };
   };
 
-  config = {
-    direnv.watchfiles = pkgs.writeText "watchfiles.txt" (lib.concatStringsSep "\n" cfg.watchFiles);
-  };
+  config.direnv.watchfiles = pkgs.writeText "watchfiles.txt" (lib.concatStringsSep "\n" cfg.watchFiles);
 }
