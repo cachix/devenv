136ca12c52af3710273aa9859e09a5a5bd78df47
Sander <hey@sandydoo.me>
2024-05-21 21:09:49 +0000

    opentelemetry-collector: init

---
+++ examples/opentelemetry-collector/.test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -ex
+
+endpoint="http://localhost:13133/"
+
+timeout 20 bash -c "until curl $endpoint 2>/dev/null; do sleep 0.5; done"
+curl -s "$endpoint" | grep "Server available"
