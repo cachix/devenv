7468708e673906bfcca9e81ea16c6cc52ea07f45
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-06-01 22:13:51 -0500

    Bump Terraform version on example

--- examples/terraform/.test.sh
+++ examples/terraform/.test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -ex
-terraform --version | grep "1.5.6"
+terraform --version | grep "1.8.4"
--- examples/terraform/devenv.nix
+++ examples/terraform/devenv.nix
@@ -1,11 +1,8 @@
 { pkgs, ... }:
 
 {
-  # For versions >= 1.6.0 open a shell by running the following command:
-  #
-  # env NIXPKGS_ALLOW_UNFREE=1 devenv --impure shell
   languages.terraform = {
     enable = true;
-    version = "1.5.6";
+    version = "1.8.4";
   };
 }
