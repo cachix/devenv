d94eade9dc766d2f3e8403dc8da90755b6b41f7d
midchildan <git@midchildan.org>
2024-06-10 05:21:26 +0900

    trafficserver: set the proxy hostname in test

--- examples/trafficserver/devenv.nix
+++ examples/trafficserver/devenv.nix
@@ -7,6 +7,7 @@
     enable = true;
     remap = "map / http://127.0.0.1 @plugin=generator.so";
     records.proxy.config = {
+      proxy_name = "devenv.test";
       http.server_ports = "8080 8080:ipv6";
       diags.logfile.filename = "stdout";
       error.logfile.filename = "stderr";
