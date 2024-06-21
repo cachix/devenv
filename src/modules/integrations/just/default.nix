b99d03bd3d8cd917164b21a76becd3a420052135
use-the-fork <sincore@gmail.com>
2024-06-20 20:01:38 -0400

    feat(integrations): add static features and modify configuration in default.nix

--- src/modules/integrations/just/default.nix
+++ src/modules/integrations/just/default.nix
@@ -11,6 +11,40 @@ let
   };
   featureType = types.submodule featureMod;
 
+  staticFeatures = {
+    convco = lib.mkOption {
+      description = "Add the 'changelog' target calling convco";
+      type = types.submodule { imports = [ featureMod ]; };
+    };
+    rust = lib.mkOption {
+      description = "Add 'w' and 'test' targets for running cargo";
+      type = types.submodule { imports = [ featureMod ]; };
+    };
+    treefmt = lib.mkOption {
+      description = "Add the 'fmt' target to format source tree using treefmt";
+      type = types.submodule { imports = [ featureMod ]; };
+    };
+    up = lib.mkOption {
+      description = "Starts processes in foreground. See http://devenv.sh/processes";
+      type = types.submodule { imports = [ featureMod ]; };
+    };
+    version = lib.mkOption {
+      description = "Display devenv version";
+      type = types.submodule { imports = [ featureMod ]; };
+    };
+  };
+
+  scriptFeatures = lib.genAttrs (builtins.attrNames config.scripts) (name:
+    let
+      script = config.scripts.${name};
+    in
+    lib.mkOption {
+      description = script.description;
+      type = types.submodule {
+        imports = [ featureMod ];
+      };
+    });
+
 in
 {
 
@@ -24,7 +58,6 @@ in
     };
   }];
 
-  # NOTE: At somepoint, we may want to add `settings` options to some of these features.
   options.just = {
     enable = lib.mkEnableOption "the just command runner";
 
@@ -43,49 +76,76 @@ in
       '';
     };
 
-    features = {
-      convco = lib.mkOption {
-        description = "Add the 'changelog' target calling convco";
-        type = types.submodule { imports = [ featureMod ]; };
-      };
-      rust = lib.mkOption {
-        description = "Add 'w' and 'test' targets for running cargo";
-        type = types.submodule { imports = [ featureMod ]; };
-      };
-      treefmt = lib.mkOption {
-        description = "Add the 'fmt' target to format source tree using treefmt";
-        type = types.submodule { imports = [ featureMod ]; };
-      };
-    };
+    features = lib.recursiveUpdate staticFeatures scriptFeatures;
   };
 
-  config = lib.mkIf config.just.enable {
+  config = {
 
     packages = [
       config.just.package
     ];
 
-    just.features = lib.mapAttrs (_: lib.mapAttrs (_: lib.mkDefault)) {
-      convco.justfile = ''
-        # Generate CHANGELOG.md using recent commits
-        changelog:
-          convco changelog -p ""
-      '';
-      rust.justfile = ''
-        # Compile and watch the project
-        w:
-          cargo watch
+    # NOTE: At somepoint, we may want to add `settings` options to some of these features.
+    just.features = lib.recursiveUpdate
+      (lib.mapAttrs (_: lib.mapAttrs (_: lib.mkDefault)) {
+        convco = {
+          enable = config.pre-commit.hooks.convco.enable;
+          justfile = ''
+            # Generate CHANGELOG.md using recent commits
+            changelog:
+              convco changelog -p ""
+          '';
+        };
+        rust = {
+          justfile = ''
+            # Compile and watch the project
+            w:
+              cargo watch
 
-        # Run and watch 'cargo test'
-        test:
-          cargo watch -s "cargo test"
-      '';
-      treefmt.justfile = ''
-        # Auto-format the source tree using treefmt
-        fmt:
-          treefmt
-      '';
-    };
+            # Run and watch 'cargo test'
+            test:
+              cargo watch -s "cargo test"
+          '';
+        };
+        treefmt = {
+          enable = config.pre-commit.hooks.treefmt.enable;
+          justfile = ''
+            # Auto-format the source tree using treefmt
+            fmt:
+              treefmt
+          '';
+        };
+        up = {
+          enable = true;
+          justfile = ''
+            # Starts processes in foreground. See http://devenv.sh/processes
+            up:
+              devenv up
+          '';
+        };
+        version = {
+          enable = true;
+          justfile = ''
+            # Display devenv version
+            version:
+              devenv version
+          '';
+        };
+      })
+      (lib.genAttrs (builtins.attrNames config.scripts) (name:
+        let
+          script = config.scripts.${name};
+        in
+        {
+          enable = script.just.enable;
+          justfile = ''
+            #${script.description}
+            ${name}:
+              ${name}
+          '';
+        }));
+
+
     enterShell =
       let
         commonJustfile = pkgs.writeTextFile {
@@ -97,7 +157,12 @@ in
       in
       ''
         ln -sf ${builtins.toString commonJustfile} ./${config.just.commonFileName}
-      '';
 
+        echo
+        echo "https://devenv.sh (version ${config.devenv.cliVersion}): Fast, Declarative, Reproducible, and Composable Developer Environments 💪💪"
+        echo
+        echo "Run 'just <recipe>' to get started"
+        just --list
+      '';
   };
 }
