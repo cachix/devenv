2dd9118064afea307ec1fa1bddd3da24e66acf71
use-the-fork <sincore@gmail.com>
2024-06-22 16:18:17 -0400

    feat(tests): add hello scripts and update just test files

--- tests/just/devenv.nix
+++ tests/just/devenv.nix
@@ -1,4 +1,25 @@
 { pkgs, ... }: {
-  just.enable = true;
-  just.recipes.convco.enable = true;
+  just = {
+    enable = true;
+    recipes = {
+      convco.enable = true;
+      hello = {
+        enable = true;
+        justfile = ''
+          # test hello
+          hello:
+            echo Hello World;
+        '';
+      };
+    };
+  };
+
+  scripts.hello-scripts = {
+    exec = ''
+      echo "Hello Script!"
+    '';
+    description = "Hello Script";
+    just.enable = true;
+  };
+
 }
