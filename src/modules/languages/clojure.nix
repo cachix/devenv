aea07abe23b73541f196c8a60620f64c9863a910
Rishi Kumar <rsi.dev17@gmail.com>
2024-07-26 19:57:20 +0530

    feat: Add option to select jdk with clojure

--- src/modules/languages/clojure.nix
+++ src/modules/languages/clojure.nix
@@ -6,12 +6,26 @@ in
 {
   options.languages.clojure = {
     enable = lib.mkEnableOption "tools for Clojure development";
+    jdk.package = lib.mkOption {
+      type = lib.types.package;
+      example = lib.literalExpression "pkgs.jdk8";
+      default = pkgs.jdk;
+      defaultText = lib.literalExpression "pkgs.jdk";
+      description = ''
+        The JDK package to use.
+        This will also become available as `JAVA_HOME`.
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
     packages = with pkgs; [
-      clojure
+      (clojure.override {
+        jdk = cfg.jdk.package;
+      })
       clojure-lsp
     ];
+
+    env.JAVA_HOME = cfg.jdk.package.home;
   };
 }
