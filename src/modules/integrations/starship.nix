8d22ca742109450f2b6be6ff451e2e7a2529eced
Patrick C. <3180910+therealpxc@users.noreply.github.com>
2024-08-09 13:20:14 -0700

    starship: don't mangle non-bash prompts for direnv

--- src/modules/integrations/starship.nix
+++ src/modules/integrations/starship.nix
@@ -34,5 +34,18 @@
       # Identify the user's terminal to call the appropiate 'starship init' command
       eval "$(starship init $(echo $0))"
     '';
+
+    # If the user's shell inherits STARSHIP_SHELL modifications from devenv,
+    # when the user's shell loads *its* Starship hooks, as happens with direnv
+    # use cases, the user's shell will can the prompt. This is because since
+    # enterShell always runs in bash, it will unconditionally set STARSHIP_SHELL
+    # to bash, which is sometimes the wrong shell. We unset it here to avoid
+    # that problem.
+    #
+    # Caveat: STARSHIP_SESSION_KEY will still be exported as it is omitted here.
+    # If the user's shell's starship process (outside of the devShell, after
+    # `direnv export`) encounters errors, it will log to the same file as the
+    # starship process run in enterShell.
+    unsetEnvVars = [ "STARSHIP_SHELL" ];
   };
 }
