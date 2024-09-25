ebd3f5f20a520bf948b143262c96fc59f412e0b3
Sander <hey@sandydoo.me>
2024-09-26 00:55:59 +0400

    package.nix: fix build error on older devenv versions

--- package.nix
+++ package.nix
@@ -4,14 +4,16 @@ pkgs.rustPlatform.buildRustPackage {
   pname = "devenv";
   version = "1.2";
 
+  # WARN: building this from src/modules/tasks.nix fails.
+  # There is something being prepended to the path, hence the .*.
   src = pkgs.lib.sourceByRegex ./. [
-    "^\.cargo(/.*)?$"
-    "Cargo\.toml"
-    "Cargo\.lock"
-    "devenv(/.*)?"
-    "tasks(/.*)?"
-    "devenv-run-tests(/.*)?"
-    "xtask(/.*)?"
+    ".*\.cargo(/.*)?$"
+    ".*Cargo\.toml"
+    ".*Cargo\.lock"
+    ".*devenv(/.*)?"
+    ".*tasks(/.*)?"
+    ".*devenv-run-tests(/.*)?"
+    ".*xtask(/.*)?"
   ];
 
   cargoBuildFlags = if build_tasks then [ "-p tasks" ] else [ "-p devenv -p devenv-run-tests" ];
