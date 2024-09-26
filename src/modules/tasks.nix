b86baebb40bd3897170a14c1ecbafc82c1ff8aba
Sander <hey@sandydoo.me>
2024-09-27 00:53:27 +0400

    chrore: add missing task option descriptions

--- src/modules/tasks.nix
+++ src/modules/tasks.nix
@@ -60,6 +60,7 @@ let
               command = config.command;
               input = config.input;
             };
+            description = "Internal configuration for the task.";
           };
           exports = lib.mkOption {
             type = types.listOf types.str;
@@ -86,13 +87,17 @@ let
   tasksJSON = (lib.mapAttrsToList (name: value: { inherit name; } // value.config) config.tasks);
 in
 {
-  options.tasks = lib.mkOption {
-    type = types.attrsOf taskType;
-  };
+  options = {
+    tasks = lib.mkOption {
+      type = types.attrsOf taskType;
+      description = "A set of tasks.";
+    };
 
-  options.task.config = lib.mkOption {
-    type = types.package;
-    internal = true;
+    task.config = lib.mkOption {
+      type = types.package;
+      internal = true;
+      description = "The generated tasks.json file.";
+    };
   };
 
   config = {
