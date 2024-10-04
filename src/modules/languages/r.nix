a96d00c110b44f3e8a5f8fa1c03b73accc6656b2
pchabros <pawel.chabros@yahoo.pl>
2024-10-04 20:40:02 +0200

    r: option to install radian console

--- src/modules/languages/r.nix
+++ src/modules/languages/r.nix
@@ -12,11 +12,18 @@ in
       defaultText = lib.literalExpression "pkgs.R";
       description = "The R package to use.";
     };
+    radian = {
+      enable = lib.mkEnableOption "a 21 century R console";
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.radianWrapper;
+        defaultText = lib.literalExpression "pkgs.radianWrapper";
+        description = "The radian package to use.";
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    packages = with pkgs; [
-      cfg.package
-    ];
+    packages = with pkgs; [ cfg.package ] ++ lib.lists.optional cfg.radian.enable cfg.radian.package;
   };
 }
