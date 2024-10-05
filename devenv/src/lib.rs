727753aceac42d60c5e378a4253c5c60de5b3298
Sander <hey@sandydoo.me>
2024-10-05 18:04:50 +0400

    chore: move tasks module to tasks crate
    This should speed up the task bin build, as it won't have to build
    everything that devenv requires.

--- devenv/src/lib.rs
+++ devenv/src/lib.rs
@@ -3,7 +3,7 @@ pub(crate) mod cnix;
 pub mod config;
 mod devenv;
 pub mod log;
-pub mod tasks;
 
 pub use cli::{default_system, GlobalOptions};
 pub use devenv::{Devenv, DevenvOptions};
+pub use devenv_tasks as tasks;
