6205194c52019c59bfa3f75dc62328cb27d82320
Sander <hey@sandydoo.me>
2024-10-13 15:53:21 +0400

    texlive: switch to new `withPackages` API

--- examples/texlive/devenv.nix
+++ examples/texlive/devenv.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   languages.texlive = {
--- src/modules/languages/texlive.nix
+++ src/modules/languages/texlive.nix
@@ -2,16 +2,15 @@
 let
   cfg = config.languages.texlive;
 
-  base = cfg.base;
-  packages = lib.genAttrs cfg.packages (name: base.${name} or (throw "No such texlive package ${name}"));
-  package = base.combine packages;
+  getPackage = ps: name: ps.${name} or (throw "No such texlive package ${name}");
+  package = cfg.base.withPackages (ps: builtins.map (getPackage ps) cfg.packages);
 in
 {
   options.languages.texlive = {
     enable = lib.mkEnableOption "TeX Live";
     base = lib.mkOption {
-      default = pkgs.texlive;
-      defaultText = lib.literalExpression "pkgs.texlive";
+      default = pkgs.texliveSmall;
+      defaultText = lib.literalExpression "pkgs.texliveSmall";
       description = "TeX Live package set to use";
     };
     packages = lib.mkOption {
