43c63cf1868f9ef2d13d2d356ddc10cb3d3dbca1
Sander <hey@sandydoo.me>
2024-12-10 08:26:45 +0400

    eval-cache: track env vars during caching

--- devenv-eval-cache/src/lib.rs
+++ devenv-eval-cache/src/lib.rs
@@ -4,5 +4,7 @@ pub(crate) mod hash;
 pub mod internal_log;
 pub mod op;
 
-pub use command::{supports_eval_caching, CachedCommand, Output};
+pub use command::{
+    supports_eval_caching, CachedCommand, EnvInputDesc, FileInputDesc, Input, Output,
+};
 pub use db::setup_db;
