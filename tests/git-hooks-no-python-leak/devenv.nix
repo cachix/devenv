7f756cdf3fbb01cab243dcec4de0ca94e6aaa2af
Sander <hey@sandydoo.me>
2025-02-05 19:22:20 +0400

    tests: fix python env leak test

--- tests/git-hooks-no-python-leak/devenv.nix
+++ tests/git-hooks-no-python-leak/devenv.nix
@@ -3,7 +3,7 @@
   git-hooks.hooks.nixfmt-rfc-style.enable = true;
 
   enterTest = ''
-    if [ -n "$PYTHONPATH" ]; then
+    if [ -n "''${PYTHONPATH:-}" ]; then
       echo "PYTHONPATH is non-empty: $PYTHONPATH" >&2
       echo "The pre-commit package is leaking its dependencies into the environment." >&2
       exit 1
