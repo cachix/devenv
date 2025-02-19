ae6ed8f3b7a4ec9e00f58d35b604b8fac42ea95a
linus <linus.f@protonmail.ch>
2025-02-19 21:27:14 +0100

    Added rudimentary test for meilisearch service.

---
+++ tests/meilisearch/devenv.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+let
+  port = 7700;
+in
+{
+
+  config = {
+    services.meilisearch = {
+      enable = true;
+      listenPort = port;
+      noAnalytics = true;
+      listenAddress = "127.0.0.1";
+    };
+
+    scripts.meilisearch-healthcheck.exec = ''
+      RUNNING=$(${pkgs.curl}/bin/curl 127.0.0.1:${toString port}/health | grep "available")
+
+      if [[ -z "$RUNNING" ]]; then
+        exit 1
+      else
+        exit 0
+      fi
+    '';
+
+    enterTest = ''
+      set -e
+
+      wait_for_port ${toString port}
+
+      # Wait for configure-mysql to finish.
+      sleep 5
+
+      meilisearch-healthcheck
+    '';
+  };
+}
