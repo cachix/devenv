04a7f9e91820d981551bd558f1552d42b510b8ab
Gabriel Nützi <gnuetzi@gmail.com>
2025-04-02 17:53:04 +0200

    fix: improve some tests

--- examples/keycloak/.test.sh
+++ examples/keycloak/.test.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 set -ex
 
-true
+echo "Startup complete..."
+echo "Checking for keycloak readiness..."
+
+for i in $(seq 1 10); do
+  if curl -v "http://localhost:8089/auth/realms/master/.well-known/openid-configuration"; then
+    echo "Keycloak master realm up and running."
+    exit 0
+  fi
+
+  echo "Could not get openid-configuration for master realm. Try: '$i/10'."
+  sleep 3
+done
+
+echo "Keycloak test failed."
+exit 1
