d2e3124587014d8d59352035ddc4869d0054e043
Christoph Hollizeck <christoph.hollizeck@hey.com>
2025-06-10 20:27:39 +0200

    zig: override option zls
    Included option to override zls e.g using a flake input

--- src/modules/languages/zig.nix
+++ src/modules/languages/zig.nix
@@ -1,4 +1,8 @@
-{ pkgs, config, lib, ... }:
+{ pkgs
+, config
+, lib
+, ...
+}:
 
 let
   cfg = config.languages.zig;
@@ -13,12 +17,19 @@ in
       default = pkgs.zig;
       defaultText = lib.literalExpression "pkgs.zig";
     };
+
+    zls.package = lib.mkOption {
+      type = lib.types.package;
+      description = "Which package of zls to use.";
+      default = pkgs.zls;
+      defaultText = lib.literalExpression "pkgs.zls";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     packages = [
       cfg.package
-      pkgs.zls
+      cfg.zls.package
     ];
   };
 }
