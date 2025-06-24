5a2511d579b6d558cc7ed2d6c12248e9944d5680
Sander <hey@sandydoo.me>
2025-06-24 21:30:41 +0200

    examples: update r example
    radian was promoted to top-level.

--- examples/r/devenv.nix
+++ examples/r/devenv.nix
@@ -1,11 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   languages.r = {
     enable = true;
-    radian = {
-      enable = true;
-      package = pkgs.python312Packages.radian;
-    };
+    radian.enable = true;
   };
 }
