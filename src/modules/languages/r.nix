a4ba9dcdc477855683e6bc13a2f9a088f89d3454
Sander <hey@sandydoo.me>
2025-06-24 19:32:28 +0200

    r: use top-level radian package if it exists
    Added in nixpkgs-unstable.

--- src/modules/languages/r.nix
+++ src/modules/languages/r.nix
@@ -6,24 +6,28 @@ in
 {
   options.languages.r = {
     enable = lib.mkEnableOption "tools for R development";
+
     package = lib.mkOption {
       type = lib.types.package;
       default = pkgs.R;
       defaultText = lib.literalExpression "pkgs.R";
       description = "The R package to use.";
     };
+
     radian = {
       enable = lib.mkEnableOption "a 21 century R console";
       package = lib.mkOption {
         type = lib.types.package;
-        default = pkgs.radianWrapper;
-        defaultText = lib.literalExpression "pkgs.radianWrapper";
+        default = pkgs.radian or pkgs.radianWrapper;
+        defaultText = lib.literalExpression "pkgs.radian or pkgs.radianWrapper";
         description = "The radian package to use.";
       };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    packages = with pkgs; [ cfg.package ] ++ lib.lists.optional cfg.radian.enable cfg.radian.package;
+    packages =
+      [ cfg.package ]
+      ++ lib.optional cfg.radian.enable cfg.radian.package;
   };
 }
