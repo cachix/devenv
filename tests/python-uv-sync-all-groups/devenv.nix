9dbd025732099cadc476922de2580750f6dd4f70
Luca <lbigon@gmail.com>
2025-07-04 09:31:00 +0200

    add test

---
+++ tests/python-uv-sync-all-groups/devenv.nix
@@ -0,0 +1,13 @@
+{ pkgs, config, inputs, ... }: {
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    uv = {
+      enable = true;
+      sync = {
+        enable = true;
+        allGroups = true;
+      };
+    };
+  };
+}
