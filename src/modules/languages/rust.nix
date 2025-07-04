8f7ad5f190ea3bea526aac2a8a17ac8688439d8e
Sander <hey@sandydoo.me>
2025-07-04 20:35:13 +0200

    lint

--- src/modules/languages/rust.nix
+++ src/modules/languages/rust.nix
@@ -17,22 +17,22 @@ let
       postBuild = ''
         for file in $(find $out/bin -xtype f -maxdepth 1); do
           install -m755 $(realpath "$file") $out/bin
-    
+
           if [[ $file =~ /rustfmt$ ]]; then
             continue
           fi
-    
+
           ${lib.optionalString pkgs.stdenv.isLinux ''
             if isELF "$file"; then
               patchelf --set-rpath $out/lib "$file" || true
             fi
           ''}
-    
+
           ${lib.optionalString pkgs.stdenv.isDarwin ''
             install_name_tool -add_rpath $out/lib "$file" || true
           ''}
         done
-    
+
         for file in $(find $out/lib -name "librustc_driver-*"); do
           install $(realpath "$file") "$file"
         done
