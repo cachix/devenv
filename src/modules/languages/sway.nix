e7eb2c8e868808aeba45c96fe370bbb74fc0da90
Mumtahin Farabi <mfarabi619@gmail.com>
2025-07-25 00:58:44 -0400

    wip
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- src/modules/languages/sway.nix
+++ src/modules/languages/sway.nix
@@ -9,17 +9,18 @@ let
     attribute = "languages.sway.input";
     follows = [ "nixpkgs" ];
   };
-  {
-    options.languages.sway = {
-      enable = lib.mkEnableOption "tools for Sway development";
+in
+{
+  options.languages.sway = {
+    enable = lib.mkEnableOption "Tools for Sway development";
 
-      components = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        default = [ "forc" "fuel-core" ];
-        defaultText = lib.literalExpression ''[ "forc" "fuel-core" ]'';
-        description = ''
+    components = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ "forc" "fuel-core" ];
+      defaultText = lib.literalExpression ''[ "forc" "fuel-core" ]'';
+      description = ''
         Blockchain domain-specific lanugage developed by the [Fuel](https://fuel.network) team as a Rust-based alternative to Solidity.
       '';
-      };
-    }
-  }
+    };
+  };
+}
