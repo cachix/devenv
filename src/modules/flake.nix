42242d877e0b3bd2494a2c798779d0749766a873
Sander <hey@sandydoo.me>
2025-08-02 15:21:30 +0200

    devenv: drop feature flag outputs entirely

--- src/modules/flake.nix
+++ src/modules/flake.nix
@@ -3,7 +3,5 @@
     { ... }:
     {
       modules = ./.;
-      isTmpDir = builtins.warn "`inputs.devenv.isTmpDir` is deprecated. Use `config.devenv.tmpdir` directly instead." true;
-      hasIsTesting = builtins.warn "`inputs.devenv.hasIsTesting` is deprecated. Use `config.devenv.isTesting` directly instead." true;
     };
 }
