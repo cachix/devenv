8c6e37efd209d55ae115073d9caa368a9870a063
Sander <hey@sandydoo.me>
2025-08-09 13:54:03 +0200

    dynamodb: fix dynamodb health-check
    dynamodb requires auth headers.
    Using the awscli is the best I could come up with

--- examples/dynamodb-local/.test.sh
+++ examples/dynamodb-local/.test.sh
@@ -5,6 +5,7 @@ export AWS_DEFAULT_REGION=fakeRegion
 export AWS_ACCESS_KEY_ID=fakeMyKeyId
 export AWS_SECRET_ACCESS_KEY=fakeSecretAccessKey
 
-wait_for_port 8000
+wait_for_processes
 
-aws dynamodb list-tables --endpoint-url http://localhost:8000
+aws dynamodb list-tables --endpoint-url http://127.0.0.1:8000 --output text --no-cli-pager
+
