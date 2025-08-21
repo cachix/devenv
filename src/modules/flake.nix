d035c387408c6680d8b1263f6d249a69cb80b8d9
Sander <hey@sandydoo.me>
2025-08-22 01:50:30 +0200

    modules: fix feature detection for older devenv versions

--- src/modules/flake.nix
+++ src/modules/flake.nix
@@ -3,5 +3,11 @@
     { ... }:
     {
       modules = ./.;
+
+      # Legacy feature-detection flags
+      # These are used by older devenv CLIs to detect certain module features that require template inputs.
+      # Deprecated as of 1.8.2
+      isTmpDir = true;
+      hasIsTesting = true;
     };
 }
