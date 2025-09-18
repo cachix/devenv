c637428d3a88ee3ceef7cbdbbb44ae533e251654
Yethal <26117918+Yethal@users.noreply.github.com>
2025-09-18 11:55:28 +0200

    starship: add inline configuration option

--- examples/starship/devenv.nix
+++ examples/starship/devenv.nix
@@ -9,6 +9,11 @@
   # If you don't want to place your configuration file next to your `devenv.yaml`,
   # change `config.path` to point to the Starship configuration file you want to use.
   #starship.config.path = ~/.config/custom_starship.toml;
+  # If you want to store your configuration within devenv.nix
+  # add it under config.settings
+  #starship.config.settings = {
+  #  add_newline = true;
+  #}
 
   # With `enableConfig` set to `false`, Starship will still look for a configuration
   # file in '~/.config/starship.toml'.
