fa3241a66d6edd84a9c685430e620cb498f812d3
Sander <hey@sandydoo.me>
2025-09-27 21:22:36 +0200

    devenv-run-tests: support disabling certain tests via the test config

--- examples/android/.patch.sh
+++
@@ -1,4 +0,0 @@
-echo "{ pkgs, lib, ... }: {"  > devenv.local.nix
-echo "  android.enable = lib.mkForce (pkgs.stdenv.isLinux && !pkgs.stdenv.isAarch64);" >> devenv.local.nix
-echo "}" >> devenv.local.nix
-
