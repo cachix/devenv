f2524e2cd6253e406c0b1f97c21d500f0214c6f3
Sander <hey@sandydoo.me>
2025-10-06 21:22:44 +0000

    wip: fix sudo issues when using tasks

--- devenv-tasks/src/privileges.rs
+++ devenv-tasks/src/privileges.rs
@@ -1,5 +1,6 @@
-use nix::unistd::{Gid, Uid, geteuid, setgid, setuid};
+use nix::unistd::{Gid, Uid, geteuid, initgroups, setgid, setuid};
 use std::env;
+use std::ffi::CString;
 
 /// Context information about the original user when running under sudo
 #[derive(Debug, Clone)]
@@ -7,6 +8,7 @@ pub struct SudoContext {
     pub user: String,
     pub uid: Uid,
     pub gid: Gid,
+    pub password: String,
 }
 
 impl SudoContext {
@@ -21,10 +23,18 @@ impl SudoContext {
         let uid = env::var("SUDO_UID").ok()?.parse().ok()?;
         let gid = env::var("SUDO_GID").ok()?.parse().ok()?;
 
+        use std::io::{self, BufRead, BufReader};
+        tracing::info!("Waiting for password");
+        let mut reader = BufReader::new(io::stdin());
+        let mut password = String::new();
+        reader.read_line(&mut password).unwrap();
+        tracing::info!("{}", password);
+
         Some(SudoContext {
             user,
             uid: Uid::from_raw(uid),
             gid: Gid::from_raw(gid),
+            password: "".to_string(),
         })
     }
 
@@ -32,8 +42,13 @@ impl SudoContext {
     ///
     /// Order matters: we must set GID first, then UID, because once we drop UID privileges we can't change GID anymore.
     pub fn drop_privileges(&self) -> Result<(), nix::Error> {
+        // Fetch and set the supplementary group access list
+        let username = CString::new(self.user.as_str()).map_err(|_| nix::Error::EINVAL)?;
+        initgroups(&username, self.gid)?;
+
         setgid(self.gid)?;
         setuid(self.uid)?;
+
         Ok(())
     }
 }
