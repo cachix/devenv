9b00e33a82632f93d773d6c72c2b050990da9f26
Domen Kožar <domen@cachix.org>
2025-10-20 14:04:53 -0500

    fix #809: add inputsFrom option

--- src/modules/top-level.nix
+++ src/modules/top-level.nix
@@ -78,6 +78,18 @@ in
       default = [ ];
     };
 
+    inputsFrom = lib.mkOption {
+      type = types.listOf types.package;
+      description = "A list of derivations whose build inputs will be merged into the shell environment.";
+      default = [ ];
+      example = lib.literalExpression ''
+        [
+          pkgs.hello
+          (pkgs.python3.withPackages (ps: [ ps.numpy ps.pandas ]))
+        ]
+      '';
+    };
+
     stdenv = lib.mkOption {
       type = types.package;
       description = "The stdenv to use for the developer environment.";
@@ -362,6 +374,7 @@ in
           inherit (config) name;
           hardeningDisable = config.hardeningDisable;
           inherit buildInputs nativeBuildInputs;
+          inputsFrom = config.inputsFrom;
           shellHook = ''
             ${lib.optionalString config.devenv.debug "set -x"}
             ${config.enterShell}
