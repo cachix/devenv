8762da63111c3695dab7db846852b42d146547ba
Johannes Müller <straightshoota@gmail.com>
2025-10-20 14:25:45 +0200

    Make Crystal and Shards packages configurable

--- src/modules/languages/crystal.nix
+++ src/modules/languages/crystal.nix
@@ -6,6 +6,20 @@ in
 {
   options.languages.crystal = {
     enable = lib.mkEnableOption "Enable tools for Crystal development.";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.crystal;
+      defaultText = lib.literalExpression "pkgs.crystal";
+      description = "The Crystal package to use.";
+    };
+
+    shards_package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.shards;
+      defaultText = lib.literalExpression "pkgs.shards";
+      description = "The Shards package to use.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -13,8 +27,8 @@ in
     languages.c.enable = lib.mkDefault true;
 
     packages = [
-      pkgs.crystal
-      pkgs.shards
+      cfg.package
+      cfg.shards_package
     ];
   };
 }
