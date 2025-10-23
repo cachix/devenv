bfc26468f6bb8eb5a75e98e21acba036475389ee
tarc <tarcisio.genaro@gmail.com>
2025-10-23 12:30:02 +0000

    doc: add treefmt examples

--- examples/formatting/devenv.nix
+++ examples/formatting/devenv.nix
@@ -1,12 +1,36 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   formatting = {
     enable = true;
 
+    treefmt.projectRootFile = "projectRootFile";
+
     treefmt.programs = {
       nixpkgs-fmt.enable = true;
       nixfmt.enable = true;
+      rustfmt.enable = true;
+    };
+  };
+
+  git-hooks.hooks = {
+    treefmt.enable = true;
+  };
+
+  formatting.treefmt.settings.formatter = {
+    "yq-json" = {
+      command = "${pkgs.bash}/bin/bash";
+      options = [
+        "-euc"
+        ''
+          for file in "$@"; do
+            ${lib.getExe pkgs.yq-go} -i --output-format=json $file
+          done
+        ''
+        "--" # bash swallows the second argument when using -c
+      ];
+      includes = [ "*.json" ];
+      excludes = [ ".git/*" ".devenv/*" ];
     };
   };
 }
