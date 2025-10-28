a7463ccdec532c311e234c202fd70f09a88cf612
Sander <hey@sandydoo.me>
2025-10-28 16:31:55 +0100

    treefmt: make config option definition conditional
    Since treefmt is an optional input, we need to make the submodule definition optional as well.
    
    This fixes tools that traverse options, like doc gen and `devenv search`.

--- src/modules/integrations/treefmt.nix
+++ src/modules/integrations/treefmt.nix
@@ -14,6 +14,16 @@ let
     follows = [ "nixpkgs" ];
   };
 
+  treefmtSubmodule =
+    let
+      optionalModule = builtins.tryEval (
+        treefmt-nix.lib.submoduleWith lib {
+          specialArgs = { inherit pkgs; };
+        }
+      );
+    in
+    if optionalModule.success then optionalModule.value else lib.types.deferredModule;
+
   # Determine tree root: prefer git.root, fallback to devenv.root
   treeRoot = if config.git.root != null then config.git.root else config.devenv.root;
 
@@ -28,9 +38,7 @@ in
 
     config = lib.mkOption {
       description = "treefmt configuration.";
-      type = treefmt-nix.lib.submoduleWith lib {
-        specialArgs = { inherit pkgs; };
-      };
+      type = treefmtSubmodule;
       default = { };
     };
   };
