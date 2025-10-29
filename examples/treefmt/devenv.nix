e350a5e4242aa6726064447c782c884c0bb82106
Sander <hey@sandydoo.me>
2025-10-29 16:18:39 +0100

    treefmt: remove unnecessary project root option for the example

--- examples/treefmt/devenv.nix
+++ examples/treefmt/devenv.nix
@@ -5,38 +5,36 @@
     pkgs.diffutils
   ];
 
+  git-hooks.hooks = {
+    treefmt.enable = true;
+  };
+
   treefmt = {
     enable = true;
-
-    config.projectRootFile = "devenv.nix";
 
     config.programs = {
       nixfmt.enable = true;
       rustfmt.enable = true;
     };
-  };
 
-  git-hooks.hooks = {
-    treefmt.enable = true;
-  };
-
-  treefmt.config.settings.formatter = {
-    "yq-json" = {
-      command = "${lib.getExe pkgs.bash}";
-      options = [
-        "-euc"
-        ''
-          for file in "$@"; do
-            ${lib.getExe pkgs.yq-go} -i --output-format=json $file
-          done
-        ''
-        "--" # bash swallows the second argument when using -c
-      ];
-      includes = [ "*.json" ];
-      excludes = [
-        ".git/*"
-        ".devenv/*"
-      ];
+    config.settings.formatter = {
+      "yq-json" = {
+        command = "${lib.getExe pkgs.bash}";
+        options = [
+          "-euc"
+          ''
+            for file in "$@"; do
+              ${lib.getExe pkgs.yq-go} -i --output-format=json $file
+            done
+          ''
+          "--" # bash swallows the second argument when using -c
+        ];
+        includes = [ "*.json" ];
+        excludes = [
+          ".git/*"
+          ".devenv/*"
+        ];
+      };
     };
   };
 }
