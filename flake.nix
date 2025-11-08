652ae94bfc7366e33d40b37e9243bb5a30e2ed08
Balint Barna Kovari <balint@kovari.cc>
2025-11-08 19:25:52 +0100

    Update after feedback

--- flake.nix
+++ flake.nix
@@ -344,7 +344,7 @@
             ci = config.ciDerivation;
             inherit config;
           };
-          nonFlakeMkShell = import ./src/non-flake-wrapper.nix self.lib.mkShell;
+        mkStandardShell = import ./src/non-flake-wrapper.nix self.lib.mkShell;
       };
 
       overlays.default = final: prev: {
--- tests/standard-nix/.test.sh
+++ tests/standard-nix/.test.sh
@@ -5,7 +5,5 @@ set -ex
 # Set up nixpkgs
 export NIX_PATH='nixpkgs=https://github.com/cachix/devenv-nixpkgs/archive/rolling.tar.gz'
 
-nix-shell -p npins --run "npins init && npins add github cachix devenv"
-
 # Verify that we've entered the shell
 nix-shell --command 'printenv IN_NON_FLAKE_SHELL'
