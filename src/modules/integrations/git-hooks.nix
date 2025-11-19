ef49f002e4b3f8107e8f0f065dd68cf7a583cb3a
Sander <hey@sandydoo.me>
2025-11-19 14:51:44 +0100

    git-hooks: support git-hooks being an optional input

--- src/modules/integrations/git-hooks.nix
+++ src/modules/integrations/git-hooks.nix
@@ -1,24 +1,76 @@
-{ pkgs, self, lib, config, inputs, ... }:
+{
+  pkgs,
+  self,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 let
   cfg = config.git-hooks;
 
-  git-hooks-module =
-    inputs.git-hooks
-      or inputs.pre-commit-hooks
-      or (throw "git-hooks or pre-commit-hooks input required");
+  inputArgs = {
+    name = "git-hooks";
+    url = "github:cachix/git-hooks.nix";
+    attribute = "git-hooks";
+    follows = [ "nixpkgs" ];
+  };
+
+  git-hooks = inputs.git-hooks or inputs.pre-commit-hooks or (config.lib.tryGetInput inputArgs);
+
+  # Check if hooks are enabled (used in config section after type is resolved)
+  hooksEnabled =
+    cfg.enable || (builtins.any (hook: hook.enable or false) (lib.attrValues (cfg.hooks or { })));
+
+  # A default module stub for when git-hooks is not available.
+  defaultModule = lib.types.submodule {
+    options = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        description = ''
+          Whether to enable the pre-commit hooks module.
+
+          When set to false, this disables the entire module.
+        '';
+        default = false;
+      };
+      hooks = lib.mkOption {
+        type = lib.types.attrsOf lib.types.attrs;
+        description = "Configuration for individual pre-commit hooks.";
+        default = { };
+      };
+    };
+  };
+
+  githooksSubmodule =
+    if git-hooks != null then
+      lib.types.submoduleWith {
+        modules = [
+          (git-hooks + "/modules/all-modules.nix")
+          {
+            rootSrc = self;
+            package = lib.mkDefault pkgs.pre-commit;
+            tools = import (git-hooks + "/nix/call-tools.nix") pkgs;
+          }
+        ];
+        specialArgs = { inherit pkgs; };
+        shorthandOnlyDefinesConfig = true;
+      }
+    else
+      defaultModule;
 
   # `propagatedBuildInputs` in Python apps are leaked into the environment.
   # This normally leaks the Python interpreter and its site-packages, causing collision errors.
   # This affects all packages built with `buildPythonApplication` or `toPythonApplication`.
   # pre-commit is particularly annoying as it is difficult for end-users to track down.
   # Tracking: https://github.com/NixOS/nixpkgs/issues/302376
-  packageBin = pkgs.runCommandLocal "pre-commit-bin" { meta.mainProgram = cfg.package.meta.mainProgram; } ''
-    mkdir -p $out/bin
-    ln -s ${lib.getExe cfg.package} $out/bin/${cfg.package.meta.mainProgram}
-  '';
-
-  anyEnabled = builtins.any (hook: hook.enable) (lib.attrValues cfg.hooks);
+  packageBin =
+    pkgs.runCommandLocal "pre-commit-bin" { meta.mainProgram = cfg.package.meta.mainProgram; }
+      ''
+        mkdir -p $out/bin
+        ln -s ${lib.getExe cfg.package} $out/bin/${cfg.package.meta.mainProgram}
+      '';
 
   # Store additional state in between evaluations to support uninstalling hooks.
   hookStateDir = "${config.devenv.state}/git-hooks";
@@ -30,24 +82,30 @@ in
   ];
 
   options.git-hooks = lib.mkOption {
-    type = lib.types.submoduleWith {
-      modules = [
-        (git-hooks-module + "/modules/all-modules.nix")
-        {
-          rootSrc = self;
-          package = lib.mkDefault pkgs.pre-commit;
-          tools = import (git-hooks-module + "/nix/call-tools.nix") pkgs;
-        }
-      ];
-      specialArgs = { inherit pkgs; };
-      shorthandOnlyDefinesConfig = true;
-    };
+    type = githooksSubmodule;
     default = { };
     description = "Integration with https://github.com/cachix/git-hooks.nix";
   };
 
   config = lib.mkMerge [
-    (lib.mkIf (!anyEnabled) {
+    # Assert that input is available when hooks are configured
+    {
+      assertions = [
+        {
+          assertion = !hooksEnabled || git-hooks != null;
+          message = ''
+            The git-hooks input is required when using git-hooks.
+
+            Run the following command to add it:
+
+              $ devenv inputs add git-hooks github:cachix/git-hooks.nix --follows nixpkgs
+
+          '';
+        }
+      ];
+    }
+
+    (lib.mkIf (!hooksEnabled) {
       # Remove the existing `configPath` if it exists and is in the nix store
       #
       # TODO(sander): turn this into a task.
@@ -75,7 +133,7 @@ in
       '';
     })
 
-    (lib.mkIf anyEnabled {
+    (lib.mkIf hooksEnabled {
       ci = [ cfg.run ];
       # Add the packages for any enabled hooks at the end to avoid overriding the language-defined packages.
       packages = lib.mkAfter ([ packageBin ] ++ (cfg.enabledPackages or [ ]));
