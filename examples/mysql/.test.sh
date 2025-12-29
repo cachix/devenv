5dbe61747ea87fa3424a5c609b4701fcf688477c
Sander <hey@sandydoo.me>
2025-12-29 17:12:33 +0100

    tests: use mariadb cli instead of the deprecated mysql wrapper

--- examples/mysql/.test.sh
+++ examples/mysql/.test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-timeout 60 bash -c 'until MYSQL_PWD="" mysql -u root test_database < /dev/null; do sleep 0.5; done'
+timeout 60 bash -c 'until MYSQL_PWD="" mariadb -u root test_database < /dev/null; do sleep 0.5; done'
