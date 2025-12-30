70d7c06e29492b13504713dbe3af14ffe856138f
Sander <hey@sandydoo.me>
2025-12-30 14:13:48 +0100

    examples: bump go version

--- examples/go/devenv.nix
+++ examples/go/devenv.nix
@@ -4,7 +4,7 @@
   packages = [ pkgs.git pkgs.gomod2nix ];
 
   languages.go.enable = true;
-  languages.go.version = "1.24.2";
+  languages.go.version = "1.25.4";
 
   git-hooks.hooks = {
     govet = {
