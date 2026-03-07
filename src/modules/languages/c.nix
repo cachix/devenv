a449543ab0b0727c208ecf4bd4ffb75e58881ca6
Sander <hey@sandydoo.me>
2026-03-07 18:56:00 +0100

    c: replace gdb with lldb as the default debugger for macOS
    lldb is what ships with XCode. gdb isn't very well tested on macOS.

--- src/modules/languages/c.nix
+++ src/modules/languages/c.nix
@@ -20,25 +20,41 @@ in
     debugger = lib.mkOption {
       type = lib.types.nullOr lib.types.package;
       default =
-        if !(pkgs.stdenv.isAarch64 && pkgs.stdenv.isLinux) && lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.gdb
+        if pkgs.stdenv.isDarwin
+        then pkgs.lldb
+        else if lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.gdb
         then pkgs.gdb
         else null;
       defaultText = lib.literalExpression "pkgs.gdb";
       description = ''
         An optional debugger package to use with c.
-        The default is `gdb`, if supported on the current system.
+        The default is `lldb` on macOS, `gdb` on Linux (if supported), or `null` otherwise.
       '';
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    packages = with pkgs; [
-      clang-tools
-      stdenv
-      gnumake
-      pkg-config
-    ] ++ lib.optional cfg.lsp.enable cfg.lsp.package
-    ++ lib.optional (cfg.debugger != null) cfg.debugger
-    ++ lib.optional (lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.valgrind && !pkgs.valgrind.meta.broken) pkgs.valgrind;
-  };
+  config = lib.mkMerge [
+    {
+      changelogs = [
+        {
+          date = "2026-03-07";
+          title = "languages.c.debugger defaults to lldb on macOS";
+          when = cfg.enable && pkgs.stdenv.isDarwin;
+          description = ''
+            The default debugger for `languages.c` on macOS has been changed from `gdb` to `lldb`.
+          '';
+        }
+      ];
+    }
+    (lib.mkIf cfg.enable {
+      packages = with pkgs; [
+        clang-tools
+        stdenv
+        gnumake
+        pkg-config
+      ] ++ lib.optional cfg.lsp.enable cfg.lsp.package
+      ++ lib.optional (cfg.debugger != null) cfg.debugger
+      ++ lib.optional (lib.meta.availableOn pkgs.stdenv.hostPlatform pkgs.valgrind && !pkgs.valgrind.meta.broken) pkgs.valgrind;
+    })
+  ];
 }
