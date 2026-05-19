16af061cd78e9862052e5244e9cabfd3f50e7f23
Gabriel Nützi <gabriel.nuetzi@sdsc.ethz.ch>
2026-05-19 17:42:58 +0200

    fix: tests

--- examples/garage/.test.sh
+++ examples/garage/.test.sh
@@ -7,17 +7,20 @@ wait_for_port "$GARAGE_S3_PORT"
 curl -sf -H "Authorization: Bearer devtoken" \
   "http://127.0.0.1:$GARAGE_ADMIN_PORT/v1/health" >/dev/null
 
+BUCKET_NAME="test-bucket"
+export GARAGE_RPC_SECRET="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
 # Test if a bucket exists
 function assert_bucket_exists() {
-  BUCKET_NAME="test-bucket"
-  export GARAGE_RPC_SECRET="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-
-  until
-    garage bucket info "$BUCKET_NAME"
-  do sleep 0.5; done
+  for _ in $(seq 1 20); do
+    if ! garage bucket info "$BUCKET_NAME"; then
+      sleep 1
+      continue
+    fi
+  done
 }
 
-if timeout 20 assert_bucket_exists; then
+if assert_bucket_exists; then
   echo "Bucket '$BUCKET_NAME' exists"
 else
   echo "Bucket '$BUCKET_NAME' does not exist"
