01fd475e266e7bf38a35d44bc1bbe7a6910691e5
tarcisio <tarcisio.genaro@gmail.com>
2026-05-21 22:35:59 +0000

    Add conan package option to lanaguages.cplusplus

--- src/modules/languages/cplusplus.nix
+++ src/modules/languages/cplusplus.nix
@@ -2,6 +2,27 @@
 
 let
   cfg = config.languages.cplusplus;
+
+  inputArgs = {
+    name = "conan-flake";
+    url = "git+https://codeberg.org/tarcisio/conan-flake";
+    attribute = "conan";
+  };
+
+  # When enabled, use getInput (throws helpful error if missing)
+  # Otherwise, use tryGetInput to populate the docs when the input is available.
+  conan-flake =
+    if cfg.conan.enable then config.lib.getInput inputArgs else config.lib.tryGetInput inputArgs;
+
+  # Determine config root: prefer git.root, fallback to devenv.root
+  configRoot = if config.git.root != null then config.git.root else config.devenv.root;
+
+  conanSubmodule =
+    if conan-flake != null then
+    # We automatically configure Conan with the correct tree root for the project.
+      conan-flake.lib.submoduleWith pkgs { inherit configRoot; }
+    else
+      lib.types.attrs;
 in
 {
   options.languages.cplusplus = {
@@ -40,6 +61,22 @@ in
       };
     };
 
+    conan = {
+      enable = lib.mkEnableOption "install conan";
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.conan;
+        defaultText = lib.literalExpression "pkgs.conan";
+        description = "The conan package to use.";
+      };
+      config = lib.mkOption {
+        type = conanSubmodule;
+        description = "conan configuration.";
+        default = { };
+      };
+      install.enable = lib.mkEnableOption "conan install during devenv initialisation";
+    };
+
     lsp = {
       enable = lib.mkEnableOption "C++ Language Server" // { default = true; };
       package = lib.mkOption {
@@ -51,12 +88,29 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    packages = with pkgs; [
-      cfg.cmake.package
-      cfg.package
-    ]
-    ++ lib.optional cfg.tools.enable cfg.tools.package
-    ++ lib.optional cfg.lsp.enable cfg.lsp.package;
-  };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      packages = with pkgs; [
+        cfg.cmake.package
+        cfg.package
+      ]
+      ++ lib.optional cfg.tools.enable cfg.tools.package
+      ++ lib.optional cfg.lsp.enable cfg.lsp.package;
+    })
+
+    #
+    (lib.mkIf (cfg.enable && cfg.conan.enable) {
+      languages.cplusplus.conan.config.stdenv = lib.mkDefault config.stdenv;
+      languages.cplusplus.conan.config.package = lib.mkDefault cfg.conan.package;
+      languages.cplusplus.conan.config.platformToolRequires = lib.mkDefault {
+        cmake = cfg.cmake.package.version;
+      };
+      languages.cplusplus.package = lib.mkDefault cfg.conan.config.stdenv.cc;
+    })
+
+    #
+    (lib.mkIf (cfg.enable && cfg.conan.enable && cfg.conan.install.enable) {
+      inputsFrom = [ cfg.conan.config.outputs.devShell ];
+    })
+  ];
 }
