1846451d1b79a0770e939779be2478709a03bd6d
tarcisio <tarcisio.genaro@gmail.com>
2026-05-22 01:04:38 +0000

    Improve docs and tests

---
+++ tests/cplusplus-conan-llvm/devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, config, ... }:
+
+{
+  languages.cplusplus = {
+    enable = true;
+    conan = {
+      enable = true;
+      install.enable = true;
+      config = {
+        stdenv = pkgs.overrideCC
+          (
+            pkgs.llvmPackages.libcxxStdenv.override {
+              targetPlatform.useLLVM = true;
+            }
+          )
+          pkgs.llvmPackages.clangUseLLVM;
+        # By default: compiler.libcxx=libstdc++11, so undo it:
+        compilerLibCxx = null;
+      };
+    };
+  };
+  enterTest = ''
+    ${pkgs.lib.getExe config.languages.cplusplus.package} --version
+    ${pkgs.lib.getExe config.languages.cplusplus.cmake.package} --version
+    ${pkgs.lib.getExe config.languages.cplusplus.conan.package} --version
+    echo "enable:"${pkgs.lib.escapeShellArg config.languages.cplusplus.tools.enable}":" | grep "enable:1:"
+    ${pkgs.lib.getExe config.languages.cplusplus.conan.package} profile show | grep "cmake/"${pkgs.lib.escapeShellArg config.languages.cplusplus.cmake.package.version}
+  '';
+}
