a6f93bb2a296a41d0ff50a6488ebf024cc99d5bf
tarcisio <tarcisio.genaro@gmail.com>
2026-05-22 11:48:01 +0000

    Improve tests

--- tests/cplusplus-conan/devenv.nix
+++ tests/cplusplus-conan/devenv.nix
@@ -6,9 +6,15 @@
   languages.cplusplus.conan.install.enable = true;
   enterTest = ''
     ${pkgs.lib.getExe config.languages.cplusplus.package} --version
+    ${pkgs.lib.getExe config.languages.cplusplus.package} --version \
+      | grep ${pkgs.lib.escapeShellArg config.stdenv.cc.cc.pname}
     ${pkgs.lib.getExe config.languages.cplusplus.cmake.package} --version
+    ${pkgs.lib.getExe config.languages.cplusplus.lsp.package} --version \
+      | grep ${pkgs.lib.escapeShellArg config.languages.cplusplus.lsp.package.version}
     ${pkgs.lib.getExe config.languages.cplusplus.conan.package} --version
-    echo "enable:"${pkgs.lib.escapeShellArg config.languages.cplusplus.tools.enable}":" | grep "enable:"${pkgs.lib.escapeShellArg config.stdenv.cc.isClang}":"
-    ${pkgs.lib.getExe config.languages.cplusplus.conan.package} profile show | grep "cmake/"${pkgs.lib.escapeShellArg config.languages.cplusplus.cmake.package.version}
+    echo "enable:"${pkgs.lib.escapeShellArg config.languages.cplusplus.tools.enable}":" \
+      | grep "enable:"${pkgs.lib.escapeShellArg config.stdenv.cc.isClang}":"
+    ${pkgs.lib.getExe config.languages.cplusplus.conan.package} profile show \
+      | grep "cmake/"${pkgs.lib.escapeShellArg config.languages.cplusplus.cmake.package.version}
   '';
 }
