2a7a8fc2e752fb2c9e8ac697cd532f5d70c8fb8c
tarcisio <tarcisio.genaro@gmail.com>
2026-05-22 13:26:23 +0000

    Test clang is default cplusplus compiler by default

--- tests/cplusplus/devenv.nix
+++ tests/cplusplus/devenv.nix
@@ -4,6 +4,8 @@
   languages.cplusplus.enable = true;
   enterTest = ''
     clang --version
+    ${pkgs.lib.getExe config.languages.cplusplus.package} --version \
+      | grep clang
     cmake --version
     ccls --version | grep ${pkgs.lib.escapeShellArg config.languages.cplusplus.lsp.package.version}
     # Validate some clang-tools are in the path:
