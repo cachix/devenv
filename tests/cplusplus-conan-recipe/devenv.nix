9509320cdcc8e514cb66b5c6ab373093346a1cd9
tarcisio <tarcisio.genaro@gmail.com>
2026-06-13 19:39:08 +0000

    Refactored cplusplus tests

--- tests/cplusplus-conan-recipe/devenv.nix
+++ tests/cplusplus-conan-recipe/devenv.nix
@@ -1,6 +1,7 @@
 { pkgs, config, ... }:
 let
   getCommand = package: builtins.baseNameOf (pkgs.lib.getExe package);
+  cfg = config.languages.cplusplus;
 in
 {
   languages.cplusplus = {
@@ -25,9 +26,11 @@ in
     };
   };
   enterTest = ''
-    ${getCommand config.languages.cplusplus.conan.package} create . --build=missing 2>&1 \
+    ${getCommand cfg.conan.package} profile show \
+      | grep "cmake/"${pkgs.lib.escapeShellArg cfg.cmake.package.version}
+    ${getCommand cfg.conan.package} create . --build=missing 2>&1 \
       | grep "example/0.0.1"
-    ${getCommand config.languages.cplusplus.conan.package} remote list \
+    ${getCommand cfg.conan.package} remote list \
       | grep "local-recipes-index, Enabled: True, Allowed packages: hello-world/0.0.1.cci.20260428"
   '';
 }
--- tests/cplusplus-conan/devenv.nix
+++ tests/cplusplus-conan/devenv.nix
@@ -1,22 +1,55 @@
 { pkgs, config, ... }:
 let
   getCommand = package: builtins.baseNameOf (pkgs.lib.getExe package);
+  cfg = config.languages.cplusplus;
 in
 {
-  languages.cplusplus.enable = true;
-  languages.cplusplus.conan.enable = true;
-  languages.cplusplus.conan.install.enable = true;
+  languages.cplusplus = {
+    enable = true;
+    conan = {
+      enable = true;
+      install.enable = false;
+      config = {
+        devShell = {
+          env.SOME_ENV_VAR = "SOME_VAL";
+          tools = {
+            inherit (pkgs) hello;
+          };
+        };
+      };
+    };
+  };
   enterTest = ''
-    ${getCommand config.languages.cplusplus.package} --version
-    ${getCommand config.languages.cplusplus.package} --version \
-      | grep ${pkgs.lib.escapeShellArg config.stdenv.cc.cc.pname}
-    ${getCommand config.languages.cplusplus.cmake.package} --version
-    ${getCommand config.languages.cplusplus.lsp.package} --version \
-      | grep ${pkgs.lib.escapeShellArg config.languages.cplusplus.lsp.package.version}
-    ${getCommand config.languages.cplusplus.conan.package} --version
-    echo "enable:"${pkgs.lib.escapeShellArg config.languages.cplusplus.tools.enable}":" \
-      | grep "enable:"${pkgs.lib.escapeShellArg config.stdenv.cc.isClang}":"
-    ${getCommand config.languages.cplusplus.conan.package} profile show \
-      | grep "cmake/"${pkgs.lib.escapeShellArg config.languages.cplusplus.cmake.package.version}
+    # Check commands are added to the path:
+    ${getCommand cfg.package} --version
+    ${getCommand cfg.cmake.package} --version
+    ${getCommand cfg.lsp.package} --version
+    ${getCommand cfg.conan.package} --version
+
+    # Check custom additions:
+    echo $SOME_ENV_VAR | grep -F "SOME_VAL"
+    hello --version
   '';
+  assertions = [
+    {
+      assertion = cfg.package.isClang -> cfg.tools.enable;
+      message = "languages.cplusplus.tools should be enabled for clang compilers by default.";
+    }
+    {
+      assertion = cfg.cmake.package == pkgs.cmake;
+      message = "by default languages.cplusplus.cmake.package == pkgs.cmake (pkgs.cmake: ${pkgs.cmake.meta.name}). Got: ${cfg.cmake.package.meta.name}";
+    }
+    {
+      assertion = cfg.conan.package == pkgs.conan;
+      message = "by default languages.cplusplus.conan.package == pkgs.conan (pkgs.conan: ${pkgs.conan.meta.name}). Got: ${cfg.conan.package.meta.name}";
+    }
+    {
+      assertion = cfg.conan.config.stdenv == config.stdenv;
+      message = "by default languages.cplusplus.conan.config.stdenv == stdenv when languages.cplusplus.conan.enable.";
+    }
+    {
+      assertion = cfg.package == cfg.conan.config.stdenv.cc;
+      message = "by default languages.cplusplus.package == languages.cplusplus.conan.config.stdenv.cc when languages.cplusplus.conan.enable (languages.cplusplus.conan.config.stdenv.cc: ${cfg.conan.config.stdenv.cc.meta.name}). Got: ${cfg.package.meta.name}";
+    }
+  ];
 }
