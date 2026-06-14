96a3d958ee7bd061682ded0fb1632c7291513471
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-06-14 10:26:44 +0200

    chore: improve the test unit to check for exitstance of the devcontainer file

--- tests/devcontainer/.test.sh
+++ tests/devcontainer/.test.sh
@@ -4,7 +4,11 @@ set -xe
 
 FILE="$PWD/.devcontainer/devcontainer.json"
 
-if [ -L "$FILE" ]; then
+if [ ! -e "$FILE" ]; then
+    echo "File does not exist"
+    exit 1
+elif [ -L "$FILE" ]; then
+    echo "File is a symlink"
     exit 1
 else
     exit 0
