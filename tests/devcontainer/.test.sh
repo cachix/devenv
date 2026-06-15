1fba4d3e4267d6dc8b078aaae8c6f57469669fb1
First-Non-Interesting-Username <janekmusin@gmail.com>
2026-06-15 19:14:07 +0200

    chore: improve test for devcontainer functionality

--- tests/devcontainer/.test.sh
+++ tests/devcontainer/.test.sh
@@ -4,12 +4,34 @@ set -xe
 
 FILE="$PWD/.devcontainer/devcontainer.json"
 
-if [ ! -e "$FILE" ]; then
-    echo "File does not exist"
+FILE_CONTENT_BEFORE_DEVENV=$(cat "$FILE")
+devenv shell --command exit
+# I'm sorry for hardcoding devcontainer-old here
+MOVED_FILE_CONTENT=$(cat $PWD/.devcontainer-old/devcontainer.json)
+
+if ["$FILE_CONTENT_BEFORE_DEVENV" != "$MOVED_FILE_CONTENT"]; then
+    echo "The content of the pre-existing devcontainer changed"
     exit 1
-elif [ -L "$FILE" ]; then
-    echo "File is a symlink"
-    exit 1
-else
-    exit 0
 fi
+
+FILE_CONTENT_AFTER_DEVENV=$(cat "$FILE")
+
+sed -i 's/DEVCONTAINER_IMAGE_PLACEHOLDER/ghcr.io\/cachix\/devenv\/devcontainer:latest/' devenv.nix
+devenv shell --command exit
+
+if ["$MOVED_FILE_CONTENT" != $(cat $PWD/.devcontainer-old/devcontainer.json)]; then
+    echo "The preserved file changed, but devcontainer was managed by devenv"
+    exit 1
+fi
+
+if ["$FILE_CONTENT_AFTER_DEVENV" == $(cat "$FILE")]; then
+    echo "Devcontainer file didn't change after changing configuration"
+    exit 1
+fi
+
+if [ -L "$FILE" ]; then
+    echo "Devcontainer file is a symlink"
+    exit 1
+fi
+
+exit 0
--- tests/devcontainer/devenv.nix
+++ tests/devcontainer/devenv.nix
@@ -1,5 +1,7 @@
-{...}: {
-  devcontainer = {
-    enable = true;
+{ pkgs, ... }:
+{
+  devcontainer.enable = true;
+  devcontainer.settings = {
+    image = "DEVCONTAINER_IMAGE_PLACEHOLDER";
   };
 }
