92491b9db5be537e156ceab2463204440d9d4fb7
tarcisio <tarcisio.genaro@gmail.com>
2026-07-08 01:06:07 +0000

    Fix test

--- examples/cplusplus-conan-cuda/.test.sh
+++ examples/cplusplus-conan-cuda/.test.sh
@@ -2,10 +2,9 @@
 
 set -ex
 
-conan install . --build=missing
-conan build . --build=missing
+conan build . --build=missing --lockfile=conan.lock
 find build -iname "example*" -type f -executable -exec "{}" ";" \
   | grep -F "example/0.0.1"
 
-conan create . --build=missing 2>&1 \
+conan create . --build=missing --lockfile=conan.lock 2>&1 \
   | grep -F "CUBLAS Matrix Multiply is close enough to CPU results: Yes"
