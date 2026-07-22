84b8f8dfe24557f27ee565ffd4d4bfdbd720d1a0
Sander <hey@sandydoo.me>
2026-07-22 14:54:52 +0200

    erlang: switch to beamPackages to fix deprecation warning

--- src/modules/languages/erlang.nix
+++ src/modules/languages/erlang.nix
@@ -2,10 +2,8 @@
 
 let
   cfg = config.languages.erlang;
-  # There's no override available
-  rebar3 = pkgs.rebar3.overrideAttrs (_: {
-    buildInputs = [ cfg.package ];
-  });
+  # rebar3 must be compiled with the selected Erlang so its BEAM files load on the user's OTP version.
+  rebar3 = (pkgs.beam.packagesWith cfg.package).rebar3;
 in
 {
   options.languages.erlang = {
@@ -14,8 +12,8 @@ in
     package = lib.mkOption {
       type = lib.types.package;
       description = "Which Erlang package to use.";
-      default = pkgs.erlang;
-      defaultText = lib.literalExpression "pkgs.erlang";
+      default = pkgs.beamPackages.erlang;
+      defaultText = lib.literalExpression "pkgs.beamPackages.erlang";
     };
 
     lsp = {
@@ -30,10 +28,26 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    packages = [
-      cfg.package
-      rebar3
-    ] ++ lib.optional cfg.lsp.enable cfg.lsp.package;
-  };
+  config = lib.mkMerge [
+    {
+      changelogs = [
+        {
+          date = "2026-07-22";
+          title = "languages.erlang.package defaults to pkgs.beamPackages.erlang";
+          when = cfg.enable;
+          description = ''
+            The default Erlang package is now `pkgs.beamPackages.erlang`, following nixpkgs' deprecation of the top-level `erlang` attribute.
+            This silences the "'erlang' is deprecated" evaluation warning on recent nixpkgs.
+            rebar3 is now built against the selected `languages.erlang.package`.
+          '';
+        }
+      ];
+    }
+    (lib.mkIf cfg.enable {
+      packages = [
+        cfg.package
+        rebar3
+      ] ++ lib.optional cfg.lsp.enable cfg.lsp.package;
+    })
+  ];
 }
