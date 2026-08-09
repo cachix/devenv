d490fed8830fb058b994f82c0410623e5cc92c61
klein.c <kleindotc@proton.me>
2026-08-09 14:59:02 -0500

    feat: Add VHDL language support
    Signed-off-by: klein.c <kleindotc@proton.me>

---
+++ src/modules/languages/vhdl.nix
@@ -0,0 +1,51 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.languages.vhdl;
+in {
+  options.languages.vhdl = {
+    enable = lib.mkEnableOption "tools for VHDL Development";
+    backend = lib.mkOption {
+      type = lib.types.enum ["ghdl-gcc" "ghdl-llvm" "ghdl-mcode" "nvc"];
+      default = "ghdl-gcc";
+      description = ''
+        The VHDL compiler backend to use.
+        - ghdl-gcc: GHDL with GCC backend
+        - ghdl-llvm: GHDL with LLVM backend
+        - ghdl-mcode: GHDL with built-in mcode backend
+        - nvc: VHDL compiler and simulator
+      '';
+    };
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.ghdl-gcc;
+      description = "The VHDL package to use (automatically set based on backend).";
+      apply = backend: let
+        packages = {
+          "ghdl-gcc" = pkgs.ghdl-gcc;
+          "ghdl-llvm" = pkgs.ghdl-llvm;
+          "ghdl-mcode" = pkgs.ghdl-mcode;
+          "nvc" = pkgs.nvc;
+        };
+      in
+        packages.${backend} or (throw "Unknown VHDL backend: ${backend}");
+    };
+
+    lsp = {
+      enable = lib.mkEnableOption "VHDL Language Server" // {default = true;};
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.vhdl-ls;
+        defaultText = lib.literalExpression "pkgs.vhdl_ls";
+        description = "The VHDL language server package to use.";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    packages = with pkgs; [cfg.package] ++ lib.optional cfg.lsp.enable cfg.lsp.package;
+  };
+}
