c890bacf745622367d1a4252f820bd8d22ccc53d
Sander <hey@sandydoo.me>
2026-08-24 17:53:32 +0200

    fix(elixir): default to the pkgs.beamPackages set
    nixpkgs deprecated the top-level `elixir` attribute.
    Default `languages.elixir.package` to `pkgs.beamPackages.elixir` and the LSP to `pkgs.beamPackages.elixir-ls`.
    This silences the "'elixir' is deprecated" evaluation warning.

--- src/modules/languages/elixir.nix
+++ src/modules/languages/elixir.nix
@@ -10,8 +10,8 @@ in
     package = lib.mkOption {
       type = lib.types.package;
       description = "Which Elixir package to use.";
-      default = pkgs.elixir;
-      defaultText = lib.literalExpression "pkgs.elixir";
+      default = pkgs.beamPackages.elixir;
+      defaultText = lib.literalExpression "pkgs.beamPackages.elixir";
     };
 
     lsp = {
@@ -19,15 +19,28 @@ in
 
       package = lib.mkOption {
         type = lib.types.package;
-        default = pkgs.elixir-ls;
-        defaultText = lib.literalExpression "pkgs.elixir-ls";
+        default = pkgs.beamPackages.elixir-ls;
+        defaultText = lib.literalExpression "pkgs.beamPackages.elixir-ls";
         description = "The Elixir language server package to use.";
       };
     };
   };
 
-  config = lib.mkIf cfg.enable
+  config = lib.mkMerge [
     {
+      changelogs = [
+        {
+          date = "2026-08-24";
+          title = "languages.elixir.package and languages.elixir.lsp.package default to the pkgs.beamPackages set";
+          when = cfg.enable;
+          description = ''
+            The default Elixir package is now `pkgs.beamPackages.elixir`, and the default Elixir language server is now `pkgs.beamPackages.elixir-ls`, following nixpkgs' deprecation of the top-level `elixir` attribute.
+            This silences the "'elixir' is deprecated" evaluation warning on recent nixpkgs.
+          '';
+        }
+      ];
+    }
+    (lib.mkIf cfg.enable {
       git-hooks.hooks = {
         credo.package = cfg.package;
         dialyzer.package = cfg.package;
@@ -38,5 +51,6 @@ in
       packages = [
         cfg.package
       ] ++ lib.optional cfg.lsp.enable cfg.lsp.package;
-    };
+    })
+  ];
 }
