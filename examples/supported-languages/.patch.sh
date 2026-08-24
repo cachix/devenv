5082fa3ac0f96967cb6a4f959bd5e00bc6b37914
Sander <hey@sandydoo.me>
2026-08-24 17:50:07 +0200

    examples: use stdenv.hostPlatform in the supported-languages patch
    nixpkgs deprecated stdenv.isLinux, stdenv.isDarwin and stdenv.isAarch64.
    This removes the evaluation warnings from the supported-languages example.

--- examples/supported-languages/.patch.sh
+++ examples/supported-languages/.patch.sh
@@ -1,18 +1,18 @@
 cat >devenv.local.nix <<EOF
 { pkgs, lib, ... }: {
   # ARM is not supported.
-  languages.unison.enable = lib.mkForce (!(pkgs.stdenv.isLinux && pkgs.stdenv.isAarch64));
-  languages.standardml.enable = lib.mkForce (!pkgs.stdenv.isAarch64);
+  languages.unison.enable = lib.mkForce (!(pkgs.stdenv.hostPlatform.isLinux && pkgs.stdenv.hostPlatform.isAarch64));
+  languages.standardml.enable = lib.mkForce (!pkgs.stdenv.hostPlatform.isAarch64);
   # https://github.com/NixOS/nixpkgs/issues/297019
-  languages.purescript.enable = lib.mkForce (!pkgs.stdenv.isAarch64);
-  android.enable = lib.mkForce (pkgs.stdenv.isLinux && !pkgs.stdenv.isAarch64);
+  languages.purescript.enable = lib.mkForce (!pkgs.stdenv.hostPlatform.isAarch64);
+  android.enable = lib.mkForce (pkgs.stdenv.hostPlatform.isLinux && !pkgs.stdenv.hostPlatform.isAarch64);
   # Doesn't build on macOS or ARM.
-  languages.odin.enable = lib.mkForce (!(pkgs.stdenv.isDarwin || (pkgs.stdenv.isLinux && pkgs.stdenv.isAarch64)));
+  languages.odin.enable = lib.mkForce (!(pkgs.stdenv.hostPlatform.isDarwin || (pkgs.stdenv.hostPlatform.isLinux && pkgs.stdenv.hostPlatform.isAarch64)));
   # macOS is broken.
-  languages.racket.enable = lib.mkForce (!pkgs.stdenv.isDarwin);
+  languages.racket.enable = lib.mkForce (!pkgs.stdenv.hostPlatform.isDarwin);
   # Swift broken on Linux with GCC 14 - https://github.com/NixOS/nixpkgs/pull/468796
-  languages.swift.enable = lib.mkForce pkgs.stdenv.isDarwin;
+  languages.swift.enable = lib.mkForce pkgs.stdenv.hostPlatform.isDarwin;
   # lobster is marked broken on macOS
-  languages.lobster.enable = lib.mkForce (!pkgs.stdenv.isDarwin);
+  languages.lobster.enable = lib.mkForce (!pkgs.stdenv.hostPlatform.isDarwin);
 }
 EOF
